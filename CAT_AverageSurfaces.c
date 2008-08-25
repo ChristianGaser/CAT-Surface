623e814bc5c3dce1d8c2b29619c4e5148d0b0d87
Christian Gaser <christian.gaser@uni-jena.de>
2008-08-25 14:10:56 +0000

    (1) Added calculation of root mean square (RMS) error to CAT_AverageSurfaces (2) Added CAT_WarpSurfSPH to use spherical harmonic coefficients to     warp a surface to a given template.
    git-svn-id: svn://141.35.69.214/CAT@51 85a05d3d-4204-4b9d-9be8-300d97d0f21f

--- CAT_AverageSurfaces.c
+++ CAT_AverageSurfaces.c
@@ -5,29 +5,56 @@
  * Copyright Christian Gaser, University of Jena.
  */
 
-/* modified from average_objects.c to enable different surface formats */
-
 #include <bicpl.h>
+#include <ParseArgv.h>
+
+/* argument defaults */
+char *outfile  = NULL;
+char *rms_file = NULL;
+
+/* the argument table */
+static ArgvInfo argTable[] = {
+  {"-avg", ARGV_STRING, (char *) 1, (char *) &outfile, 
+     "Average."},
+  {"-rms", ARGV_STRING, (char *) 1, (char *) &rms_file, 
+     "Root mean square error."},
+   {NULL, ARGV_END, NULL, NULL, NULL}
+};
+
+void
+usage(char *executable)
+{
+        char *usage_str = "\n\
+Usage: %s -avg average.obj|-rms rms_file.txt input1.obj input2.obj [... inputN.obj]\n\n\
+     Calculate average and root mean square error of surfaces.\n\n";
+
+        fprintf(stderr, usage_str, executable);
+}
 
 int
 main(int argc, char *argv[])
 {
         Status           status;
-        char             *infile, *outfile;
+        char             *infile;
         int              i, n_objects;
+        FILE             *fp;
         File_formats     format;
-        int              n_sets, n_pts, n_set_pts;
+        int              n_sets, n_pts, n_avg_pts;
         object_struct    *out_object;
         object_struct    **object_list;
-        Point            *pts, *set_pts;
+        Point            *pts, *avg_pts, *sqr_pts, sqr_pt;
+        double           value;
+
+        /* Call ParseArgv */
+        if (ParseArgv(&argc, argv, argTable, 0) ||
+            (rms_file == NULL && outfile == NULL)) {
+                fprintf(stderr, "Either average or rms file must be defined.\n");
+                usage(argv[0]);
+                fprintf(stderr, "     %s -help\n\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
 
         initialize_argument_processing(argc, argv);
-
-        if (!get_string_argument(NULL, &outfile)) {
-            fprintf(stderr, "Usage: %s output\n", argv[0]);
-            fprintf(stderr, "         [input1] [input2] ...\n");
-            return(1);
-        }
 
         n_sets = 0;
 
@@ -42,16 +69,25 @@ main(int argc, char *argv[])
 
                 if (n_sets == 0) {
                         out_object = object_list[0];
-                        n_set_pts = n_pts;
-                        set_pts = pts;
+                        n_avg_pts = n_pts;
+                        avg_pts = pts;
+                        if (rms_file != NULL) {
+                                sqr_pts = (Point *) malloc(sizeof(Point) * n_pts);
+                                for (i = 0; i < n_pts; i++)
+                                        POINT_EXP2(sqr_pts[i], pts[i], *, pts[i]);
+                        }
                 } else {
-                        if (n_pts != n_set_pts) {
+                        if (n_pts != n_avg_pts) {
                                 printf("N points mismatch\n");
                                 return(1);
                         }
 
                         for (i = 0; i < n_pts; i++) {
-                                ADD_POINTS(set_pts[i], set_pts[i], pts[i]);
+                                ADD_POINTS(avg_pts[i], avg_pts[i], pts[i]);
+                                if (rms_file != NULL) {
+                                        POINT_EXP2(sqr_pt, pts[i], *, pts[i]);
+                                        ADD_POINTS(sqr_pts[i], sqr_pts[i], sqr_pt);
+                                }
                         }
                 }
 
@@ -64,19 +100,36 @@ main(int argc, char *argv[])
         }
 
         if (n_sets == 0) {
-	        fprintf(stderr, "No input?!  No output for you!!\n");
-	        return(1);
+                fprintf(stderr, "No input?!  No output for you!!\n");
+                return(1);
         }
 
         for (i = 0; i < n_pts; i++) {
-	        SCALE_POINT(set_pts[i], set_pts[i], (1.0/n_sets));
+                SCALE_POINT(avg_pts[i], avg_pts[i], (1.0/n_sets));
         }
 
-        if (get_object_type(out_object) == POLYGONS)
-                compute_polygon_normals(get_polygons_ptr(out_object));
+        if (outfile != NULL) {
+                if (get_object_type(out_object) == POLYGONS)
+                        compute_polygon_normals(get_polygons_ptr(out_object));
 
-        status = output_graphics_any_format(outfile, ASCII_FORMAT,
+                status = output_graphics_any_format(outfile, ASCII_FORMAT,
                                             1, &out_object);
+        }
+        
+        if (rms_file != NULL) {
+                if (open_file(rms_file, WRITE_FILE, ASCII_FORMAT, &fp) != OK)
+                        return(1);
+                for (i = 0; i < n_pts; i++) {
+                        SCALE_POINT(sqr_pts[i], sqr_pts[i], (1.0/(n_sets-1.0)));
+                        POINT_EXP2(avg_pts[i], avg_pts[i], *, avg_pts[i]);
+                        SCALE_POINT(avg_pts[i], avg_pts[i], (n_sets/(n_sets-1.0)));
+                        SUB_POINTS(sqr_pts[i], sqr_pts[i], avg_pts[i]);
+                        value = sqrt(Point_x(sqr_pts[i]) + Point_y(sqr_pts[i]) + Point_z(sqr_pts[i]));
+                        if (output_real(fp, value) != OK ||
+                            output_newline(fp) != OK)
+                                break;
+                }
+                free(sqr_pts);
+        }
 
-        return(status != OK);
 }
