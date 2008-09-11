f21d4a21f368d38c30435f2dc4ac5f1cfdeb9617
Rachel A. Yotter <raytrace@gmail.com>
2008-09-11 11:17:08 +0000

    Converted CAT_LBConformalMap to C rather than C++.  It runs around 5 times faster.  There is also a subtle difference between the values returned by the NL solver, which then propagates.  Since the code is exactly the same, the returned values should not be different!
    git-svn-id: svn://141.35.69.214/CAT@62 85a05d3d-4204-4b9d-9be8-300d97d0f21f

--- CAT_LBConformalMap.c
+++ CAT_LBConformalMap.c
@@ -41,31 +41,19 @@
  */
 /****************************************************************************/
 
-#undef MOEBIUS
-
-extern "C"
-{   
-        #include "nl/nl.h"
-        #include <volume_io/internal_volume_io.h>
-        #include <bicpl.h>
-        #include <CAT_Curvature.h>
-        #include <CAT_Blur2d.h>
-        #include <CAT_Surf.h>
-        #include <CAT_SurfaceIO.h>
-}   
-
+#include <volume_io/internal_volume_io.h>
+#include <bicpl.h>
 #include <ParseArgv.h>
-
-#include <vector>
-#include <set>
-#include <string>
-#include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <cfloat>
-
 #include <math.h>
 #include <assert.h>
+#include <float.h>
+
+#include "nl/nl.h"
+#include "CAT_Curvature.h"
+#include "CAT_Blur2d.h"
+#include "CAT_Surf.h"
+#include "CAT_SurfaceIO.h"
+
 
 struct ptmap {
         int *cells;
@@ -162,9 +150,9 @@ moebius_transform(double *zR, double *zI, double *factor,
         pts = (Point *) malloc(sizeof(Point) * polygons->n_points);
 
         /* first get all of the areas of the original triangles */
-        for (f = 0; f < polygons->n_items; f++) {
-                int size = get_polygon_points(polygons, f, tpts);
-                mareas[f] = get_polygon_surface_area(size, tpts);
+        for (i = 0; i < polygons->n_items; i++) {
+                size = get_polygon_points(polygons, i, tpts);
+                mareas[i] = get_polygon_surface_area(size, tpts);
         }
 
         /* get an estimate of the optimal factor value */
@@ -200,7 +188,6 @@ moebius_transform(double *zR, double *zI, double *factor,
                 }
                 fstep /= 2;
         }
-        printf("f = %f, ad: %f\n", *factor, areadist);
 
         /* shift */
         fstep = 0.001;
@@ -242,7 +229,7 @@ moebius_transform(double *zR, double *zI, double *factor,
                 }
                 fstep /= 2;
         }
-        printf("zRf = %f, zIf = %f, ad: %f\n", *zRf, *zIf, areadist);
+        printf("factor = %f, zRf = %f, zIf = %f\n", *factor, *zRf, *zIf);
         free(mareas);
         free(areas);
         free(pts);
@@ -363,7 +350,7 @@ mesh_to_solver()
 {
         int npts = polygons->n_points;
         int nfacets = polygons->n_items;
-        int i, f, n, p, pidx, size;
+        int i, j, f, n, p, q, r, s, pidx, size;
         int  **cellinfo;
         int  *n_neighbours, **neighbours;
         struct ptmap *ptinfo;
@@ -371,16 +358,6 @@ mesh_to_solver()
         create_polygon_point_neighbours(polygons, TRUE, &n_neighbours,
                                         &neighbours, NULL, NULL);
 
-        // 1. store the points coordinates: pointXYZ
-        std::vector< std::vector<double> > pointXYZ(npts, std::vector<
-                                                          double>(3, 0) );
-
-        for (p = 0; p < npts; p++) {
-              pointXYZ[p][0] = polygons->points[p].coords[0];
-              pointXYZ[p][1] = polygons->points[p].coords[1];
-              pointXYZ[p][2] = polygons->points[p].coords[2];            
-        }
-          
         /* find point closest to center-of-mass */
         if (ptP < 0)
                 ptP = findPointP();
@@ -396,11 +373,11 @@ mesh_to_solver()
 
         ptinfo = (struct ptmap *) malloc(sizeof(struct ptmap) * npts);
         cellinfo = (int **) malloc(sizeof(int *) * nfacets);
+
         for (p = 0; p < npts; p++)
                 ptinfo[p].size = 0;
         for (f = 0; f < nfacets; f++)
                 cellinfo[f] = (int *) malloc(sizeof(int) * 3);
-        std::vector <std::vector<int> > pointCell(npts);
 
         for (f = 0; f < nfacets; f++) {
             size = GET_OBJECT_SIZE(*polygons, f);
@@ -413,31 +390,27 @@ mesh_to_solver()
                                                          n_neighbours[pidx]);
                 ptinfo[pidx].cells[ptinfo[pidx].size] = f;
                 ptinfo[pidx].size++;
-                pointCell[pidx].push_back(f);
                 cellinfo[f][p] = pidx;
             }
         }
 
-        std::cerr << "  Checking the existence of boundary...";
-        for (p = 0; p < npts; p++) {
-            if (pointCell[p].size() < 3) {
-                    // If one node has <= 2 neighbors, it's on the boundary.
-                    // This is the sufficient condition, i.e., it may still be a
-                    // boundary point even having more than 3 neighbors.
-                    // So, what's the equivalent expression of being a boundary point?
-                    std::cerr<<"There is boundary in mesh! exiting..."<<std::endl;
-                    exit(-1);
-            }
-        }
-        std::cerr<<"No boundary found!"<<std::endl;
-        
         // compute b = bR + i*bI separately
-        std::vector<double> bR(npts), bI(npts);
-        int facet = pointCell[ptP][0];
+        double *bR, *bI;
+        int facet = ptinfo[ptP].cells[0];
 
-        std::vector<double> A( pointXYZ[ cellinfo[facet][0] ] ),
-                            B( pointXYZ[ cellinfo[facet][1] ] ),
-                            C( pointXYZ[ cellinfo[facet][2] ] );
+        bR = (double *) malloc(sizeof(double) * npts);
+        bI = (double *) malloc(sizeof(double) * npts);
+
+        for (i = 0; i < npts; i++) {
+                bR[i] = bI[i] = 0;
+        }
+
+        double A[3], B[3], C[3];
+        for (i = 0; i < 3; i++) {
+              A[i] = polygons->points[ cellinfo[facet][0] ].coords[i];
+              B[i] = polygons->points[ cellinfo[facet][1] ].coords[i];
+              C[i] = polygons->points[ cellinfo[facet][2] ].coords[i];
+        }
         double ABnorm, CA_BAip; // the inner product of vector C-A and B-A;
         ABnorm = (A[0] - B[0]) * (A[0] - B[0]) +
                  (A[1] - B[1]) * (A[1] - B[1]) +
@@ -446,14 +419,14 @@ mesh_to_solver()
         CA_BAip = (C[0] - A[0]) * (B[0] - A[0]) +
                   (C[1] - A[1]) * (B[1] - A[1]) +
                   (C[2] - A[2]) * (B[2] - A[2]);
-        
+
         double theta = CA_BAip / ABnorm;
         // Here ABnorm is actually the square of AB's norm, which is what we
         // want. So don't bother square the square root.
         
         ABnorm = sqrt(ABnorm); // This is real norm of vector AB.
         
-        std::vector<double> E(3);
+        double E[3];
         for (i = 0; i < 3; i++)
                 E[i] = A[i] + theta*(B[i] - A[i]);
         
@@ -469,175 +442,127 @@ mesh_to_solver()
         bI[cellinfo[facet][0]] = (1 - theta) / CEnorm;
         bI[cellinfo[facet][1]] = theta / CEnorm;
         bI[cellinfo[facet][2]] = -1 / CEnorm;
-        
+
         // 1. Iterate point P from 0 to the last point in the mesh.
         // 2. For each P, find its neighbors
         // 3. For each of P's neighbors, Q, calculate R, S
         // 4. Write the value in matrix.
-        
-        std::vector< std::vector<int> >::iterator itP, itPEnd = pointCell.end();
-        int idP = 0;
-        unsigned long numOfEdges = 0;
 
         progress_struct progress;
-        initialize_progress_report(&progress, FALSE, npts, "Mesh2Solver");
-        for (itP = pointCell.begin(); itP != itPEnd; ++itP, ++idP) {
-            update_progress_report(&progress, idP);
-            std::vector<int> neighborOfP;
-            std::vector<double> Dr(npts*2); // matrix row values
-            // for each point P, traverse all cells containing it.
-            std::vector<int>::iterator itCellEnd = (*itP).end();
-            
-            for (f = 0; f < ptinfo[idP].size; f++) {
-                facet = ptinfo[idP].cells[f];
-                // for each cell containing P, store the point with larger point Id.
-                // only three points, don't use for-loop to save time.
-                if (cellinfo[facet][0] != idP)
-                    neighborOfP.push_back(cellinfo[facet][0]);
-                if (cellinfo[facet][1] != idP)
-                    neighborOfP.push_back(cellinfo[facet][1]);
-                if (cellinfo[facet][2] != idP)
-                    neighborOfP.push_back(cellinfo[facet][2]);
-            } // for itCell. Ok, now all neighbors of P is stored in neighborOfP
+        double *Dr;
 
-            sort(neighborOfP.begin(), neighborOfP.end());
-            std::vector<int>::iterator it;
-            it = unique(neighborOfP.begin(), neighborOfP.end());
-            neighborOfP.erase(it, neighborOfP.end());
-            
+        Dr = (double *) malloc(sizeof(double) * npts * 2);
 
-            // //-----------------------------------------------
-            // // print out the neighbors
-            // std::vector<int>::iterator itNeighbor = neighborOfP.begin();
-            // std::vector<int>::iterator itNeighborEnd = neighborOfP.end();
-            // std::cerr<<"The neighbors of "<<idP<<" are: ";
-            // for (; itNeighbor != itNeighborEnd; ++itNeighbor) {
-            //     std::cerr<<*itNeighbor<<" , ";
-            // }
-            // std::cerr<<std::endl;
-            // // ----------------------------------------------------
-            
-            
-            // next, from P to each neighbor...
-            // note: itP and itQ point at different type of vectors...
-            // *itP is a vector containing a list of cell Ids, all of which contains point P
-            // idP is the point Id of P
-            // *itQ is the point Id of Q (so idP and *itQ are same type)
-            std::vector<int>::iterator itQ, itQEnd = neighborOfP.end();
-            for (itQ = neighborOfP.begin(); itQ != itQEnd; ++itQ) {
-                if (*itQ > idP)
-                    numOfEdges++;
+        initialize_progress_report(&progress, FALSE, polygons->n_points,
+                                   "Mesh2Solver");
 
-                // first check whether PQ is a boundary edge:
-                std::vector<int> cellsContainingP(*itP), cellsContainingQ(pointCell[*itQ]);
-                std::vector<int> cells(cellsContainingP.size() + cellsContainingQ.size());
-                std::vector<int>::iterator itv, endIter;
-                
-                sort(cellsContainingP.begin(), cellsContainingP.end());
-                sort(cellsContainingQ.begin(), cellsContainingQ.end());
-                
-                endIter = set_intersection(cellsContainingP.begin(), cellsContainingP.end(),
-                                           cellsContainingQ.begin(), cellsContainingQ.end(),
-                                           cells.begin());
-                cells.erase(endIter, cells.end());
-                if (cells.size() != 2) continue;
-                // If P and Q are not shared by two triangles, i.e. 1: are not
-                // connected by and edge, or, 2: are on the surface boundary
-                // thus only shared by one triangle. then skip.  However, in
-                // this paper the surface is closed thus there is not boundary.
-                
-                // If passed test above, then P and Q are two valid points.
-                // i.e. PQ is a valid edge.  i.e. cells now contain two int's,
-                // which are the Id of the triangles containing P and Q
-                
-                
-                // //------------------------------------------------------------
-                // //print out valid edge
-                // std::cerr<<idP<<" and "<<*itQ<<" are two valid points"<<std::endl;
-                // std::cerr<<(endIter == cells.end())<<std::endl;
-                // //-----------------------------------------------------------
-                
-                
-                // Next we extract R and S from cells
-                int itS, itR; // the Id of point S and R;
+        for (p = 0; p < polygons->n_points; p++) {
+                for (i = 0; i < npts * 2; i++)
+                        Dr[i] = 0;
+                if (n_neighbours[p] <= 1)
+                        continue; /* skip this point */
 
-                for (int it = 0; it < 3; ++it) {
-                    if (cellinfo[cells[0]][it] != idP && cellinfo[cells[0]][it] != *itQ)
-                        itS = cellinfo[cells[0]][it];
-                    if (cellinfo[cells[1]][it] != idP && cellinfo[cells[1]][it] != *itQ) 
-                        itR = cellinfo[cells[1]][it];
+                for (n = 0; n < n_neighbours[p]; n++) {
+                        q = neighbours[p][n];
+
+                        /* find the two triangles containing both P and Q */
+                        int facets[2] = {-1, -1};
+                        for (i = 0; i < ptinfo[p].size; i++) {
+                                for (j = 0; j < ptinfo[q].size; j++) {
+                                        if (ptinfo[p].cells[i] == ptinfo[q].cells[j]) {
+                                                if (facets[0] < 0)
+                                                        facets[0] = ptinfo[p].cells[i];
+                                                else
+                                                        facets[1] = ptinfo[p].cells[i];
+                                        }
+                                }
+                        }
+        
+                        /* get R and S */
+                        size = GET_OBJECT_SIZE(*polygons, facets[0]);
+                        for (i = 0; i < size; i++) {
+                                pidx = polygons->indices[POINT_INDEX(polygons->end_indices,
+                                                     facets[0], i)];
+                                if (pidx != p && pidx != q)
+                                        r = pidx;
+                        }
+                        size = GET_OBJECT_SIZE(*polygons, facets[1]);
+                        for (i = 0; i < size; i++) {
+                                pidx = polygons->indices[POINT_INDEX(polygons->end_indices,
+                                                     facets[1], i)];
+                                if (pidx != p && pidx != q)
+                                        s = pidx;
+                        }
+
+                        double P[3], Q[3], R[3], S[3];
+
+                        for (i = 0; i < 3; i++) {
+                                P[i] = polygons->points[p].coords[i];
+                                Q[i] = polygons->points[q].coords[i];
+                                R[i] = polygons->points[r].coords[i];
+                                S[i] = polygons->points[s].coords[i];
+                        }
+                
+                        double SP[3], SQ[3], RP[3], RQ[3];
+                        double SPnorm = 0, SQnorm = 0, RPnorm = 0;
+                        double RQnorm = 0, SPSQinnerProd = 0, RPRQinnerProd = 0;
+
+                        for (i = 0; i < 3; ++i) {
+                                SP[i] = P[i] - S[i];
+                                SPnorm += SP[i] * SP[i];
+
+                                SQ[i] = Q[i] - S[i];
+                                SQnorm += SQ[i] * SQ[i];
+                                SPSQinnerProd += SP[i] * SQ[i];
+
+                                RP[i] = P[i] - R[i];
+                                RPnorm += RP[i] * RP[i];
+
+                                RQ[i] = Q[i] - R[i];
+                                RQnorm += RQ[i] * RQ[i];
+                                RPRQinnerProd += RP[i] * RQ[i];
+                        }
+                        SPnorm = sqrt(SPnorm);
+                        SQnorm = sqrt(SQnorm);
+                        RPnorm = sqrt(RPnorm);
+                        RQnorm = sqrt(RQnorm);
+                
+                        double cosS = SPSQinnerProd / (SPnorm * SQnorm);
+                        double cosR = RPRQinnerProd / (RPnorm * RQnorm);
+                        double ctgS = cosS / sqrt(1 - cosS*cosS);
+                        double ctgR = cosR / sqrt(1 - cosR*cosR);
+
+                        Dr[q] = -0.5 * (ctgS + ctgR);
+                        Dr[p] += 0.5 * (ctgS + ctgR);
+                        // add to the diagonal element of this line.
+                
+                        //D(*itQ, idP) = -0.5*(ctgS + ctgR); // symmetric
+                        //D(*itQ, *itQ) += 0.5*(ctgS + ctgR);
                 }
 
-                std::vector<double> P(pointXYZ[idP]),
-                        Q(pointXYZ[*itQ]),
-                        R(pointXYZ[itR]),
-                        S(pointXYZ[itS]);
-                
-                std::vector<double> SP(3), SQ(3), RP(3), RQ(3);
-                double SPnorm = 0, SQnorm = 0, RPnorm = 0, RQnorm = 0, SPSQinnerProd = 0, RPRQinnerProd = 0;
-                for (int it = 0; it < 3; ++it) {
-                    SP[it] = P[it] - S[it];
-                    SPnorm += SP[it] * SP[it];
+                // write the real values
+                nlRowParameterd(NL_RIGHT_HAND_SIDE, bR[p]);
+                nlBegin(NL_ROW);
 
-                    SQ[it] = Q[it] - S[it];
-                    SQnorm += SQ[it] * SQ[it];
-                    SPSQinnerProd += SP[it] * SQ[it];
-
-                    RP[it] = P[it] - R[it];
-                    RPnorm += RP[it] * RP[it];
-
-                    RQ[it] = Q[it] - R[it];
-                    RQnorm += RQ[it] * RQ[it];
-                    RPRQinnerProd += RP[it] * RQ[it];
+                for (i = 0; i < npts; i++) {
+                        if (Dr[i] != 0) { // store it
+                                nlCoefficient(i*2, Dr[i]);
+                        }
                 }
-                SPnorm = sqrt(SPnorm);
-                SQnorm = sqrt(SQnorm);
-                RPnorm = sqrt(RPnorm);
-                RQnorm = sqrt(RQnorm);
-                
-                double cosS = SPSQinnerProd / (SPnorm * SQnorm);
-                double cosR = RPRQinnerProd / (RPnorm * RQnorm);
-                double ctgS = cosS / sqrt(1 - cosS*cosS);
-                double ctgR = cosR / sqrt(1 - cosR*cosR);
-                
-                Dr[*itQ] = -0.5 * (ctgS + ctgR);
-                Dr[idP] += 0.5 * (ctgS + ctgR);
-                // add to the diagonal element of this line.
-                
-                //D(*itQ, idP) = -0.5*(ctgS + ctgR); // symmetric
-                //D(*itQ, *itQ) += 0.5*(ctgS + ctgR);
-            }
+                nlEnd(NL_ROW);
             
-            // write the real values
-            nlRowParameterd(NL_RIGHT_HAND_SIDE, bR[idP]);
-            nlBegin(NL_ROW);
-
-            for (int i = 0; i < npts; i++) {
-                if (Dr[i] != 0) { // store it
-                    nlCoefficient(i*2, Dr[i]);
+                // write the imaginary values
+                nlRowParameterd(NL_RIGHT_HAND_SIDE, -bI[p]);
+                nlBegin(NL_ROW);
+                for (i = 0; i < npts; i++) {
+                        if (Dr[i] != 0) {
+                                nlCoefficient(i*2 + 1, Dr[i]);
+                        }
                 }
-            }
-            nlEnd(NL_ROW);
-            
-            // write the imaginary values
-            nlRowParameterd(NL_RIGHT_HAND_SIDE, -bI[idP]);
-            nlBegin(NL_ROW);
-            for (int i = 0; i < npts; i++) {
-                if (Dr[i] != 0) {
-                    nlCoefficient(i*2 + 1, Dr[i]);
-                }
-            }
-            nlEnd(NL_ROW);
+                nlEnd(NL_ROW);
+                update_progress_report(&progress, p);
         }
-
         terminate_progress_report(&progress);
-
-        /* calc Euler number to test whether mesh is genus 0 (Euler num = 2) */
-        int eulerNum = npts - numOfEdges + nfacets;
-
-        fprintf(stderr, "  Calculating Euler characteristics......\n");
-        fprintf(stderr, "    Euler Characteristics = %d\n", eulerNum);
-        fprintf(stderr, "    genus = %d\n", (2.0 - eulerNum)/2);
 }
 
 /* Apply the Laplace-Beltrami operator */
