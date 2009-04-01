6b3f726821aa85f667fecb2d462fefc1a8de5054
Christian Gaser <christian.gaser@uni-jena.de>
2009-04-01 14:27:26 +0000

    Corrected output of betas in CAT_GLM_Estimate.c
    git-svn-id: svn://141.35.69.214/CAT@102 85a05d3d-4204-4b9d-9be8-300d97d0f21f

--- CAT_GLM_Estimate.c
+++ CAT_GLM_Estimate.c
@@ -7,7 +7,6 @@
  *
  */
 
-#include <volume_io/internal_volume_io.h>
 #include <bicpl.h>
 #include <time_stamp.h>
 
@@ -18,20 +17,7 @@
 #define DEBUG 1
 
 /* Definitions for accessing information on each dimension */
-#define NUM_DIMENSIONS 3        /* Number of dimensions of volume */
 #define MAX_FILES 1500          /* Number of possible files */
-
-/* Types */
-typedef struct {
-   long nslices;                     /* Number of slices */
-   long nrows;                       /* Number of rows in inputdata */
-   long ncolumns;                    /* Number of columns in inputdata */
-   double maximum;                   /* Volume maximum */
-   double minimum;                   /* Volume minimum */
-   double step[NUM_DIMENSIONS];    /* Step sizes for dimensions */
-   double start[NUM_DIMENSIONS];    /* Start positions for dimensions */
-   char dimension_names[NUM_DIMENSIONS][MAX_NC_NAME]; /* Dimension names */
-} Volume_Info;
 
 void
 usage(char *executable)
@@ -103,21 +89,12 @@ estimate(char **infiles, char *arg_string, int argc)
         FILE                 *fp;
         char                 buffer[1024];
         int                  i, j, k, n_subj, n_vals, n_tmp, prev_n_vals;
-        int                  n_beta, rank, erdf, counter;
-        int                  in_icvid[MAX_FILES], out_beta_icvid[MAX_FILES];
-        int                  out_T_icvid[MAX_FILES], out_res_icvid[MAX_FILES];
-        int                  out_resMS_icvid, *idx;
-        int                  islice, n_dims, n_cov;
-        double               *outdata;
+        int                  n_beta, rank, erdf, counter, *idx;
+        int                  n_cov;
         Real                 **vals, *tmpvals, *data, **indata;
         Real                 **G, *v, **inv_G, **transp_G, **pinv_GG;
-        Real                 **beta, **estimates, **resSD, sum, *result;
+        Real                 **beta, *beta0, **estimates, **resSD, sum, *result;
         progress_struct      progress;
-        Volume_Info          vol_info[MAX_FILES];
-
-        if (filename_extension_matches(infiles[0], "mnc"))
-                n_dims = 3;
-        else    n_dims = 1;
 
         counter = 0;
         n_beta = 1;
@@ -141,11 +118,6 @@ estimate(char **infiles, char *arg_string, int argc)
                                         infiles[i]);
                                 return(1);
                         }
-                        /* read input icvid for minc files */
-                        if (n_dims == 3)
-                                in_icvid[counter] = get_volume_info(infiles[i],
-                                                                   &vol_info[i],
-                                                                   NC_DOUBLE);
                         counter++;
                 }
         }
@@ -162,8 +134,7 @@ estimate(char **infiles, char *arg_string, int argc)
         }
 
         /* read data and build design matrix */
-        if (n_dims == 1)
-                printf("Read files:");
+        printf("Read files:");
         fflush(stdout);
     
         counter = 0;
@@ -192,7 +163,7 @@ estimate(char **infiles, char *arg_string, int argc)
                 } else if (equal_strings(infiles[i], "+")) {
                         /* define groups */
                         j++;
-                } else if (n_dims == 1) {
+                } else {
                         if (input_values_any_format(infiles[i], &n_vals,
                                                  &tmpvals) != OK) {
                                 fprintf(stderr, "\nError reading file %s\n",
@@ -212,22 +183,13 @@ estimate(char **infiles, char *arg_string, int argc)
                         prev_n_vals = n_vals;
                         G[counter][j] = 1;
                         counter++;
-                } else if (n_dims == 3) {
-                        G[counter][j] = 1;
-                        counter++;
                 }
 
-                if (n_dims == 1) {
-                        printf(".");
-                        fflush(stdout);
-                }
+                printf(".");
+                fflush(stdout);
         }
 
-        if (n_dims == 1)
-                printf("\n");
-
-        if (n_dims == 3)
-                n_vals = vol_info[0].nrows * vol_info[0].ncolumns;
+        printf("\n");
     
         /* compute pseudo inverse from design matrix */        
         rank = pinv(n_subj, n_beta, G, inv_G);
@@ -275,8 +237,8 @@ estimate(char **infiles, char *arg_string, int argc)
         fclose(fp);
 
         ALLOC(data, n_vals);
-        ALLOC(outdata, n_vals);
         ALLOC(v, n_vals);
+        ALLOC(beta0, n_vals);
         ALLOC2D(resSD, n_beta, n_vals);
         ALLOC2D(indata, n_subj, n_vals);
         ALLOC2D(estimates, n_subj, n_vals);
@@ -290,227 +252,77 @@ estimate(char **infiles, char *arg_string, int argc)
         (void) pinv(n_beta, n_beta, pinv_GG, pinv_GG);
 
         /* ---------------------------------------------------------------- */
-        /*  estimation for minc-files */
-        /* ---------------------------------------------------------------- */
-        if (n_dims == 3) {
-                /* Check vol info about dimensions, voxel size and origin */
-                for (i = 1; i < n_subj; i++) {
-                        if (vol_info[idx[i]].nslices != vol_info[0].nslices ||
-                            vol_info[idx[i]].nrows != vol_info[0].nrows ||
-                            vol_info[idx[i]].ncolumns != vol_info[0].ncolumns) {
-                                fprintf(stderr,
-                                        "Dimensions of file %s differ.\n",
-                                        infiles[idx[i]]);
-                                return(1);
-                        }
-                        if (vol_info[idx[i]].step[0] != vol_info[0].step[0] ||
-                            vol_info[idx[i]].step[1] != vol_info[0].step[1] ||
-                            vol_info[idx[i]].step[2] != vol_info[0].step[2]) {
-                                fprintf(stderr,
-                                        "Voxel size of file %s differs.\n",
-                                        infiles[idx[i]]);
-                                return(1);
-                        }
-                        if (vol_info[idx[i]].start[0] != vol_info[0].start[0] ||
-                            vol_info[idx[i]].start[1] != vol_info[0].start[1] ||
-                            vol_info[idx[i]].start[2] != vol_info[0].start[2]) {
-                                fprintf(stderr,
-                                        "Origin of file %s differs.\n",
-                                        infiles[idx[i]]);
-                                return(1);
-                        }
-                }
-
-                /* prepare output for resMS */
-                outfile = create_string("ResMS.mnc");
-                out_resMS_icvid = save_volume_info(in_icvid[0], outfile,
-                                                   arg_string, &vol_info[0],
-                                                   NC_FLOAT);
-
-                /* prepare output files for beta-, T-, and ResSD-images */
-                for (j = 0; j < n_beta; j++) {
-                        outfile = create_string("beta");
-                        sprintf(buffer, "_%04d.mnc", j+1);
-                        concat_to_string(&outfile, buffer);
-                        out_beta_icvid[j] = save_volume_info(in_icvid[0],
-                                                             outfile,
-                                                             arg_string, 
-                                                             &vol_info[0],
-                                                             NC_FLOAT);
-                        outfile = create_string("T");
-                        sprintf(buffer, "_%04d.mnc", j+1);
-                        concat_to_string(&outfile, buffer);
-                        out_T_icvid[j] = save_volume_info(in_icvid[0], outfile,
-                                                          arg_string, 
-                                                          &vol_info[0],
-                                                          NC_FLOAT);
-                        outfile = create_string("ResSD");
-                        sprintf(buffer, "_%04d.mnc", j+1);
-                        concat_to_string(&outfile, buffer);
-                        out_res_icvid[j] = save_volume_info(in_icvid[0],
-                                                            outfile, arg_string,
-                                                            &vol_info[0],
-                                                            NC_FLOAT);
-                }
-
-                initialize_progress_report(&progress, FALSE,
-                                           vol_info[0].nslices, "Estimate GLM");
-
-                /* Loop through slices */
-                for (islice = 0; islice < vol_info[0].nslices; islice++) {
-                        /* get input data */    
-                        for (i = 0; i < n_subj; i++) {
-                                get_volume_slice_double(in_icvid[i],
-                                                        &vol_info[idx[i]],
-                                                        islice, data);
-                                for (k = 0; k < n_vals; k++)
-                                        indata[i][k] = data[k];
-                        }
-
-                        /* calculate beta by multiplying pseudo inverse */
-                        /* from design matrix with transposed data */    
-                        matrix_multiply(n_beta, n_subj, n_vals, inv_G,
-                                        indata, beta);
-
-                        /* calculate fitted data: estimates = G*beta */
-                        matrix_multiply(n_subj, n_beta, n_vals, G, beta,
-                                        estimates);
-
-                        /* calculate estimated residual standard deviation: */
-                        /* ResSD = sqrt(sum(res.^2)/erdf) */
-                        for (k = 0; k < n_vals; k++) {
-                                sum = 0.0;
-                                for (i = 0; i < n_subj; i++) {
-                                        estimates[i][k] = indata[i][k] -
-                                                          estimates[i][k];
-                                        sum += estimates[i][k] *
-                                               estimates[i][k];
-                                }
-                                v[k] = sum / (Real) erdf;
-                        }
-
-                        for (k = 0; k < n_vals; k++)
-                                outdata[k] = (double) v[k];
-                        save_volume_slice_double(out_resMS_icvid, &vol_info[0],
-                                                 islice, outdata);
-
-                        for (j = 0; j < n_beta; j++) {
-                                for (k = 0; k < n_vals; k++)
-                                        resSD[j][k] = sqrt(v[k] *
-                                                           pinv_GG[j][j]);
-                        }
-
-                        /* save slices */
-                        for (j = 0; j < n_beta; j++) {
-                                for (k = 0; k < n_vals; k++)
-                                        outdata[k] = (double) resSD[j][k];
-
-                                save_volume_slice_double(out_res_icvid[j],
-                                                         &vol_info[0], islice,
-                                                         outdata);
-                                for (k = 0; k < n_vals; k++)
-                                        outdata[k] = (double) beta[j][k];
-
-                                save_volume_slice_double(out_beta_icvid[j],
-                                                         &vol_info[0], islice,
-                                                         outdata);
-                                for (k = 0; k < n_vals; k++)
-                                        outdata[k] = (double) beta[j][k] /
-                                                     (resSD[j][k] + EPS);
-
-                                save_volume_slice_double(out_T_icvid[j],
-                                                         &vol_info[0], islice,
-                                                         outdata);
-                        }
-                        update_progress_report(&progress, islice + 1);    
-                }  // islice++
-                terminate_progress_report(&progress);
-
-                /* Free up data and close files */
-                close_volume(out_resMS_icvid);
-                for (i = 0; i < n_subj; i++)
-                        close_volume(in_icvid[i]);
-
-                for (j = 0; j < n_beta; j++) {
-                        close_volume(out_beta_icvid[j]);
-                        close_volume(out_T_icvid[j]);
-                }
-        }  // if n_dims == 3
-
-        /* ---------------------------------------------------------------- */
         /*  estimation for ascii-files */
         /* ---------------------------------------------------------------- */
 
-        if (n_dims == 1) {
-                /* multiply pseudo inverse from design matrix with */
-                /* transposed data */    
-                for (k = 0; k < n_vals; k++) {
-                        for (j = 0; j < n_beta; j++) {
-                                sum = 0.0;
-                                for (i = 0; i < n_subj; i++)
-                                sum += inv_G[j][i] * vals[i][k];        
-                                beta[j][k] = sum;
-                        }
-                }
-
-                /* write betas for each column of design matrix */
+        /* multiply pseudo inverse from design matrix with */
+        /* transposed data */    
+        for (k = 0; k < n_vals; k++) {
                 for (j = 0; j < n_beta; j++) {
-                        outfile = create_string("beta");
-                        sprintf(buffer, "_%04d.txt", j+1);
-                        concat_to_string(&outfile, buffer);
-    
-                        output_values_any_format(outfile, n_vals, beta);
-                }
-
-                /* calculate fitted data: estimates = G*beta */
-                matrix_multiply(n_subj, n_beta, n_vals, G, beta, estimates);
-
-                /* calculate estimated squared residual standard deviation: */
-                /* v = sum((vals - estimates).^2)/erdf */
-                for (k = 0; k < n_vals; k++) {
                         sum = 0.0;
-                        for (i = 0; i < n_subj; i++) {
-                                estimates[i][k] = vals[i][k] - estimates[i][k];
-                                sum += estimates[i][k] * estimates[i][k];
-                        }
-                        v[k] = sum / erdf;
+                        for (i = 0; i < n_subj; i++)
+                                sum += inv_G[j][i] * vals[i][k];        
+                        beta[j][k] = sum;
                 }
+        }
 
-                outfile = create_string("ResMS.txt");    
-                output_values_any_format(outfile, n_vals, v);
+        /* write betas for each column of design matrix */
+        for (j = 0; j < n_beta; j++) {
+                outfile = create_string("beta");
+                sprintf(buffer, "_%04d.txt", j+1);
+                concat_to_string(&outfile, buffer);
+                for (k = 0; k < n_vals; k++) beta0[k] = beta[j][k];
+    
+                output_values_any_format(outfile, n_vals, beta0);
+        }
 
-                /* write beta and beta/ResSD for each column of design matrix */
-                for (j = 0; j < n_beta; j++) {
-                        /* ResSD values*/
-                        outfile = create_string("ResSD");
-                        sprintf(buffer, "_%04d.txt", j+1);
-                        concat_to_string(&outfile, buffer);
-                        
-                        ALLOC(result, n_vals);
-                            
-                        for (k = 0; k < n_vals; k++) {
-                                result[k] = sqrt(v[k] * pinv_GG[j][j]);
-                        }
-                        output_values_any_format(outfile, n_vals, result);
+        /* calculate fitted data: estimates = G*beta */
+        matrix_multiply(n_subj, n_beta, n_vals, G, beta, estimates);
 
-                        /* T-values */
-                        outfile = create_string("T");
-                        sprintf(buffer, "_%04d.txt", j+1);
-                        concat_to_string(&outfile, buffer);
-
-                        for (k = 0; k < n_vals; k++) {
-                                result[k] = beta[j][k] /
-                                         (sqrt(v[k] * pinv_GG[j][j]) + EPS);
-                        }
-                        output_values_any_format(outfile, n_vals, result);
-                        FREE(result);
+        /* calculate estimated squared residual standard deviation: */
+        /* v = sum((vals - estimates).^2)/erdf */
+        for (k = 0; k < n_vals; k++) {
+                sum = 0.0;
+                for (i = 0; i < n_subj; i++) {
+                        estimates[i][k] = vals[i][k] - estimates[i][k];
+                        sum += estimates[i][k] * estimates[i][k];
                 }
-        } // if n_dims == 1
+                v[k] = sum / erdf;
+        }
+
+        outfile = create_string("ResMS.txt");    
+        output_values_any_format(outfile, n_vals, v);
+
+        /* write beta and beta/ResSD for each column of design matrix */
+        for (j = 0; j < n_beta; j++) {
+                /* ResSD values*/
+                outfile = create_string("ResSD");
+                sprintf(buffer, "_%04d.txt", j+1);
+                concat_to_string(&outfile, buffer);
+                
+                ALLOC(result, n_vals);
+                    
+                for (k = 0; k < n_vals; k++) {
+                        result[k] = sqrt(v[k] * pinv_GG[j][j]);
+                }
+                output_values_any_format(outfile, n_vals, result);
+
+                /* T-values */
+                outfile = create_string("T");
+                sprintf(buffer, "_%04d.txt", j+1);
+                concat_to_string(&outfile, buffer);
+
+                for (k = 0; k < n_vals; k++) {
+                        result[k] = beta[j][k] / (sqrt(v[k] * pinv_GG[j][j]) + EPS);
+                }
+                output_values_any_format(outfile, n_vals, result);
+                FREE(result);
+        }
     
         FREE(data);
-        FREE(outdata);
         FREE(v);
         FREE(idx);
+        FREE(beta0);
         FREE2D(resSD);
         FREE2D(indata);
         FREE2D(estimates);
