d4cbbe0b794f4e5e80ee257d703d8bc8e4c299ba
Rachel A. Yotter <raytrace@gmail.com>
2009-04-14 12:06:43 +0000

    Changed things so that it only does the forward transform once, then band-limits the result to generate two surfaces that are then combined... makes it go faster and improves the quality of the final surface.
    git-svn-id: svn://141.35.69.214/CAT@111 85a05d3d-4204-4b9d-9be8-300d97d0f21f

--- CAT_FixTopology.c
+++ CAT_FixTopology.c
@@ -15,14 +15,15 @@
 #include "CAT_Blur2d.h"
 #include "CAT_SPH.h"
 
-#define BW1 128
-#define BW2 1024
+#define BW 1024
+#define LIM 64
 #define N_TRIANGLES 327680
 #define DATAFORMAT 1 /* 1 = real data, 0 = complex data */
 
 /* argument defaults */
 double t1_threshold = 0.0;
-char *t1_file   = NULL;
+char *t1_file = NULL;
+BOOLEAN gauss_smooth = 0; /* gaussian curvature weighted smoothing on/off */
 
 /* the argument table */
 ArgvInfo argTable[] = {
@@ -35,6 +36,9 @@ ArgvInfo argTable[] = {
   { "-th", ARGV_FLOAT, (char *) 1, 
     (char *) &t1_threshold,
     "Threshold between GM and CSF." },
+  { "-smooth", ARGV_CONSTANT, (char *) TRUE, 
+    (char *) &gauss_smooth,
+    "Turn on Gaussian curvature weighted smoothing (recommended for CS)." },
   { NULL, ARGV_END, NULL, NULL, NULL }
 };
 
@@ -46,68 +50,6 @@ Usage: %s surface.obj sphere.obj output.obj\n\
 Correct the topology of a brain surface.\n\\n\n";
 
        fprintf(stderr, usage_str, executable);
-}
-
-object_struct *
-get_sph_object(polygons_struct *polygons, polygons_struct *sphere,
-               int bw, int bw_lim)
-{
-        double *rcx, *icx, *rcy, *icy, *rcz, *icz;
-        double *rdatax, *rdatay, *rdataz;
-        polygons_struct *sph_polygons;
-        object_struct *objects;
-        int bw2, i;
-
-        bw2 = bw * bw;
-
-        rdatax = (double *) malloc(sizeof(double) * 4 * bw2);
-        rdatay = (double *) malloc(sizeof(double) * 4 * bw2);
-        rdataz = (double *) malloc(sizeof(double) * 4 * bw2);
-        rcx    = (double *) malloc(sizeof(double) * bw2);
-        rcy    = (double *) malloc(sizeof(double) * bw2);
-        rcz    = (double *) malloc(sizeof(double) * bw2);
-        icx    = (double *) malloc(sizeof(double) * bw2);
-        icy    = (double *) malloc(sizeof(double) * bw2);
-        icz    = (double *) malloc(sizeof(double) * bw2);
-
-        get_equally_sampled_coords_of_polygon(polygons, sphere, bw,
-                                              rdatax, rdatay, rdataz);
-
-        get_sph_coeffs_of_realdata(rdatax, bw, DATAFORMAT, rcx, icx);
-        get_sph_coeffs_of_realdata(rdatay, bw, DATAFORMAT, rcy, icy);
-        get_sph_coeffs_of_realdata(rdataz, bw, DATAFORMAT, rcz, icz);
-
-        if (bw_lim > 0 && bw_lim < bw) {
-                fprintf(stderr,"Limit BW..");
-                butterworth_filter(bw, bw_lim, rcx, rcx);
-                butterworth_filter(bw, bw_lim, rcy, rcy);
-                butterworth_filter(bw, bw_lim, rcz, rcz);
-                butterworth_filter(bw, bw_lim, icx, icx);
-                butterworth_filter(bw, bw_lim, icy, icy);
-                butterworth_filter(bw, bw_lim, icz, icz);
-        }
-    
-        get_realdata_from_sph_coeffs(rdatax, bw, DATAFORMAT, rcx, icx);
-        get_realdata_from_sph_coeffs(rdatay, bw, DATAFORMAT, rcy, icy);
-        get_realdata_from_sph_coeffs(rdataz, bw, DATAFORMAT, rcz, icz);
-
-        objects = create_object(POLYGONS);
-        sph_polygons = get_polygons_ptr(objects);
-
-        sample_sphere_from_sph(rdatax, rdatay, rdataz,
-                               sph_polygons, N_TRIANGLES, bw);
-
-        free(rcx);
-        free(rcy);
-        free(rcz);
-        free(icx);
-        free(icy);
-        free(icz);
-        free(rdatax);
-        free(rdatay);
-        free(rdataz);
-
-        return objects;
 }
 
 static int
@@ -125,65 +67,171 @@ compare(const void *a, const void *b)
 
 void
 set_weights_of_neighbours(int **neighbours, int *n_neighbours, int p,
-                          double *sharpness, double *weights)
+                          double *sharpness, double *weights, int level)
 {
         int n, idx;
         double w;
 
+        if (level == 0)
+                return;
+
         for (n = 0; n < n_neighbours[p]; n++) {
                 idx = neighbours[p][n];
-                if (weights[idx] == 1 || sharpness[idx] < 45)
+
+                if (weights[idx] == 1)
                         continue;
 
                 weights[idx] = 1;
                 set_weights_of_neighbours(neighbours, n_neighbours, idx,
-                                          sharpness, weights);
+                                          sharpness, weights, level - 1);
         }
+}
+
+void
+smooth_topology_sph(polygons_struct *polygons, polygons_struct *smoothed_polys)
+{
+        double *sharpness, *weights;
+        Point *smooth_pts;
+        Real value;
+        int *n_neighbours, **neighbours;
+        int i, p;
+
+        create_polygon_point_neighbours(polygons, TRUE, &n_neighbours,
+                                        &neighbours, NULL, NULL);
+
+        smooth_pts = (Point *) malloc(sizeof(Point) * polygons->n_points);
+        sharpness = (double *) malloc(sizeof(double) * polygons->n_points);
+        weights = (double *) malloc(sizeof(double) * polygons->n_points);
+        memset(weights, 0.0, sizeof(double) * polygons->n_points);
+
+        compute_local_sharpness(polygons, n_neighbours, neighbours, sharpness);
+
+        for (p = 0; p < polygons->n_points; p++) {
+                if (sharpness[p] > 110.0) {
+                        weights[p] = 1;
+                        set_weights_of_neighbours(neighbours, n_neighbours, p,
+                                                  sharpness, weights, 2);
+                }
+        }
+
+        /* combine the surfaces based on the weights */
+        for (p = 0; p < polygons->n_points; p++) {
+                for (i = 0; i < 3; i++) {
+                        Point_coord(polygons->points[p], i) =
+                            ((1 - weights[p]) * Point_coord(polygons->points[p],i)) +
+                            ((    weights[p]) * Point_coord(smoothed_polys->points[p],i));
+                }
+        }
+
+        /* remove any additional peaks caused by the above step */
+        compute_local_sharpness(polygons, n_neighbours, neighbours, sharpness);
+
+        for (p = 0; p < polygons->n_points; p++) {
+                if (sharpness[p] > 110.0 && weights[p] == 0) {
+                        for (i = 0; i < 3; i++) {
+                                Point_coord(polygons->points[p], i) =
+                                      Point_coord(smoothed_polys->points[p],i);
+                        }
+                }
+        }
+
+        /* Gaussian curvature smoothing */
+        if (gauss_smooth) {
+                get_polygon_vertex_curvatures_cg(polygons, n_neighbours, neighbours,
+                                                 0.0, 1, weights);
+                for (p = 0; p < polygons->n_points; p++)
+                        weights[p] = (fabs(weights[p]) > 0.01)
+                                     ? 5.0*fabs(weights[p]) : 0.0;
+
+                for (p = 0; p < polygons->n_points; p++) {
+                        /* smooth only if gaussian curv strength is > 0 */
+                        if (weights[p] > 0.0) {
+                                heatkernel_blur_points(polygons->n_points,
+                                                       polygons->points, NULL,
+                                                       n_neighbours[p],
+                                                       neighbours[p],
+                                                       p, weights[p],
+                                                       &smooth_pts[p],
+                                                       &value);
+                        } else smooth_pts[p] = polygons->points[p];
+                }
+                for (p = 0; p < polygons->n_points; p++)
+                        polygons->points[p] = smooth_pts[p];
+        }
+
+        free(smooth_pts);
+        free(sharpness);
+        free(weights);
 }
 
 object_struct *
 fix_topology_sph(polygons_struct *polygons, polygons_struct *sphere)
 {
-        object_struct *bw1_objects, *bw2_objects;
-        polygons_struct *bw1, *bw2;
-        double *sharpness, *weights;
-        int *n_neighbours, **neighbours;
-        int i, p, count;
+        object_struct *objects, *lim_objects;
+        polygons_struct *sph_polys, *lim_polys;
+        double *rcx, *icx, *rcy, *icy, *rcz, *icz;
+        double *lrcx, *licx, *lrcy, *licy, *lrcz, *licz;
+        double *rdatax, *rdatay, *rdataz;
+        int bw2;
 
-        bw1_objects = get_sph_object(polygons, sphere, BW1, BW1);
-        bw1 = get_polygons_ptr(bw1_objects);
-        bw2_objects = get_sph_object(polygons, sphere, BW2, BW2);
-        bw2 = get_polygons_ptr(bw2_objects);
+        bw2 = BW * BW;
 
-        create_polygon_point_neighbours(bw2, TRUE, &n_neighbours,
-                                        &neighbours, NULL, NULL);
+        rdatax = (double *) malloc(sizeof(double) * 4 * bw2);
+        rdatay = (double *) malloc(sizeof(double) * 4 * bw2);
+        rdataz = (double *) malloc(sizeof(double) * 4 * bw2);
+        rcx    = (double *) malloc(sizeof(double) * bw2);
+        rcy    = (double *) malloc(sizeof(double) * bw2);
+        rcz    = (double *) malloc(sizeof(double) * bw2);
+        icx    = (double *) malloc(sizeof(double) * bw2);
+        icy    = (double *) malloc(sizeof(double) * bw2);
+        icz    = (double *) malloc(sizeof(double) * bw2);
+        lrcx   = (double *) malloc(sizeof(double) * bw2);
+        lrcy   = (double *) malloc(sizeof(double) * bw2);
+        lrcz   = (double *) malloc(sizeof(double) * bw2);
+        licx   = (double *) malloc(sizeof(double) * bw2);
+        licy   = (double *) malloc(sizeof(double) * bw2);
+        licz   = (double *) malloc(sizeof(double) * bw2);
 
-        sharpness = (double *) malloc(sizeof(double) * bw1->n_points);
-        weights = (double *) malloc(sizeof(double) * bw1->n_points);
-        memset(weights, 0.0, sizeof(double) * bw1->n_points);
+        get_equally_sampled_coords_of_polygon(polygons, sphere, BW,
+                                              rdatax, rdatay, rdataz);
 
-        compute_local_sharpness(bw2, n_neighbours, neighbours, sharpness);
+        get_sph_coeffs_of_realdata(rdatax, BW, DATAFORMAT, rcx, icx);
+        get_sph_coeffs_of_realdata(rdatay, BW, DATAFORMAT, rcy, icy);
+        get_sph_coeffs_of_realdata(rdataz, BW, DATAFORMAT, rcz, icz);
 
-        for (p = 0; p < bw2->n_points; p++) {
-                if (sharpness[p] < 70.0)
-                        continue;
+        get_realdata_from_sph_coeffs(rdatax, BW, DATAFORMAT, rcx, icx);
+        get_realdata_from_sph_coeffs(rdatay, BW, DATAFORMAT, rcy, icy);
+        get_realdata_from_sph_coeffs(rdataz, BW, DATAFORMAT, rcz, icz);
 
-                weights[p] = 1;
-                set_weights_of_neighbours(neighbours, n_neighbours, p,
-                                          sharpness, weights);
-        }
+        objects = create_object(POLYGONS);
+        sph_polys = get_polygons_ptr(objects);
+        sample_sphere_from_sph(rdatax, rdatay, rdataz,
+                               sph_polys, N_TRIANGLES, BW);
 
-        /* combine the surfaces based on the weights */
-        count = 0;
-        for (p = 0; p < bw2->n_points; p++) {
-                for (i = 0; i < 3; i++) {
-                        Point_coord(bw2->points[p], i) =
-                            ((1 - weights[p]) * Point_coord(bw2->points[p],i)) +
-                            ((    weights[p]) * Point_coord(bw1->points[p],i));
-                }
-        }
+        butterworth_filter(BW, LIM, rcx, lrcx);
+        butterworth_filter(BW, LIM, rcy, lrcy);
+        butterworth_filter(BW, LIM, rcz, lrcz);
+        butterworth_filter(BW, LIM, icx, licx);
+        butterworth_filter(BW, LIM, icy, licy);
+        butterworth_filter(BW, LIM, icz, licz);
+    
+        get_realdata_from_sph_coeffs(rdatax, BW, DATAFORMAT, lrcx, licx);
+        get_realdata_from_sph_coeffs(rdatay, BW, DATAFORMAT, lrcy, licy);
+        get_realdata_from_sph_coeffs(rdataz, BW, DATAFORMAT, lrcz, licz);
 
-        return bw2_objects;
+        lim_objects = create_object(POLYGONS);
+        lim_polys = get_polygons_ptr(lim_objects);
+        sample_sphere_from_sph(rdatax, rdatay, rdataz,
+                               lim_polys, N_TRIANGLES, BW);
+
+        free(rcx); free(rcy); free(rcz);
+        free(icx); free(icy); free(icz);
+        free(lrcx); free(lrcy); free(lrcz);
+        free(licx); free(licy); free(licz);
+        free(rdatax); free(rdatay); free(rdataz);
+
+        smooth_topology_sph(sph_polys, lim_polys);
+        return objects;
 }
 
 int
