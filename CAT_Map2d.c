eb606808e1bc2010277454da5d3bf56bed1eabc6
Rachel A. Yotter <raytrace@gmail.com>
2011-06-01 14:04:42 +0000

    Added new functions: (1) wrap/unwrap sheet2d and (2) smooth a sheet2d.  The wrapping functions assume that the values are related to u/v values.
    git-svn-id: svn://141.35.69.214/CAT@203 85a05d3d-4204-4b9d-9be8-300d97d0f21f

--- CAT_Map2d.c
+++ CAT_Map2d.c
@@ -18,6 +18,7 @@
 #define  BINTREE_FACTOR   0.5
 #define  NEW_COORDINATE_SYSTEM   1
 
+
 double
 compute_clockwise_rotation2(double x, double y)
 {
@@ -93,64 +94,129 @@ uv_to_point(double u, double v, Point *point)
         fill_Point(*point, x, y, z);
 }
 
+
 void
-map_smoothed_curvature_to_sphere(polygons_struct *polygons, polygons_struct *sphere, double *values,
-                                 double *data, double fwhm, int *size_map, int curvtype)
+wrap_sheet2d(double *data, int *dm, int wrap, double *wdata)
+{
+        int x, y, x0, y0, wdm[2];
+
+        wdm[0] = dm[0] + 2*wrap;
+        wdm[1] = dm[1] + 2*wrap;
+
+        for (x = 0; x < dm[0]; x++) {
+                for (y = 0; y < dm[1]; y++) {
+                        x0 = x + wrap;
+                        y0 = y + wrap;
+                        wdata[x0 + (wdm[0]*y0)] = data[x + (dm[0]*y)];
+                }
+        }
+
+        for (y = 0; y < wrap; y++) {
+                for (x = wrap; x < wdm[0] - wrap; x++) {
+                        x0 = (int) ((x - wrap + dm[0]/2) % dm[0]) + wrap;
+                        y0 = 2*wrap - y - 1;
+                        wdata[x + wdm[0]*y] = wdata[x0 + wdm[0]*y0];
+                        y0 = wdm[1] - 2*wrap + y;
+                        wdata[x + wdm[0]*(wdm[1] - y - 1)] =
+                                                          wdata[x0 + wdm[0]*y0];
+                }
+        }
+
+        for (x = 0; x < wrap; x++) {
+                for (y = 0; y < wdm[1]; y++) {
+                        x0 = wdm[0] - 2*wrap + x;
+                        wdata[x + wdm[0]*y] = wdata[x0 + wdm[0]*y];
+                        x0 = wrap + x;
+                        wdata[x + wdm[0] - wrap + wdm[0]*y] =
+                                                          wdata[x0 + wdm[0]*y];
+                }
+        }
+}
+
+
+void
+unwrap_sheet2d(double *wdata, int *wdm, int wrap, double *data)
+{
+        int x, y, x0, y0, dm[2];
+
+        dm[0] = wdm[0] - 2*wrap;
+        dm[1] = wdm[1] - 2*wrap;
+
+        for (x = 0; x < dm[0]; x++) {
+                for (y = 0; y < dm[1]; y++) {
+                        x0 = x + wrap;
+                        y0 = y + wrap;
+                        data[x + dm[0]*y] = wdata[x0 + wdm[0]*y0];
+                }
+        }
+
+        for (y = 0; y < wrap; y++) {
+                for (x = wrap; x < wdm[0] - wrap; x++) {
+                        x0 = (int) ((x - wrap + dm[0]/2) % dm[0]);
+                        y0 = wrap - y - 1;
+                        data[x0 + dm[0]*y0] += wdata[x + wdm[0]*y];
+                        if (x0 > wrap - 1 && x0 < dm[0] - wrap)
+                                data[x0 + dm[0]*y0] /= 2;
+                        y0 = dm[1] - wrap + y;
+                        data[x0 + dm[0]*y0] += wdata[x + wdm[0]*(wdm[1] - y-1)];
+                        if (x0 > wrap - 1 && x0 < dm[0] - wrap)
+                                data[x0 + dm[0]*y0] /= 2;
+                }
+        }
+
+        for (x = 0; x < wrap; x++) {
+                for (y = 0; y < dm[1]; y++) {
+                        data[x + dm[0]*y] += wdata[x + wdm[0] - wrap +
+                                                   wdm[0]*(y + wrap)];
+                        x0 = dm[0] - x - 1;
+                        data[x0 + dm[0]*y] += wdata[wrap - x  - 1 +
+                                                    wdm[0]*(y + wrap)];
+                        if (y > wrap - 1 && y < dm[1] - wrap) {
+                                data[x + dm[0]*y] /= 2;
+                                data[x0 + dm[0]*y] /= 2;
+                        }
+                }
+        }
+
+
+        for (x = 0; x < wrap; x++) {
+                for (y = 0; y < wrap; y++) {
+                        x0 = dm[0] - x - 1;
+                        y0 = dm[1] - y - 1;
+                        data[x  + dm[0]*y ] /= 3;
+                        data[x0 + dm[0]*y ] /= 3;
+                        data[x  + dm[0]*y0] /= 3;
+                        data[x0 + dm[0]*y0] /= 3;
+                }
+        }
+}
+
+
+void
+map_smoothed_curvature_to_sphere(polygons_struct *polygons,
+                                 polygons_struct *sphere, double *values,
+                                 double *data, double fwhm, int *dm,
+                                 int curvtype)
 {
         polygons_struct   unit_sphere;
         Point             unit_point, on_sphere_point, centre;
         Point             poly_points[1000];
-        double            sigma, value, *smooth_values;
-        double            u, v;
-        double            weights[1000], mn, mx, distance;
-        int               *n_neighbours, **neighbours;
+        double            sigma, value;
+        double            u, v, wm;
+        double            weights[1000], mn, mx;
         int               i, j, n_iter;
         int               x, y, validx;
         int               poly, size, ind;
 
-
-        get_all_polygon_point_neighbours(polygons, &n_neighbours, &neighbours);
-
         /* if values is empty calculate curvature */
         if (values == (double *)0) {
-                values = (double *)malloc(sizeof(double)*polygons->n_points);
-                if (curvtype == 0)
-                        distance = 3.0;
-                else distance = 0.0;
-
-                get_polygon_vertex_curvatures_cg(polygons, n_neighbours, neighbours,
-                                         distance, curvtype, values);
-        }
-        smooth_values = (double *)malloc(sizeof(double)*polygons->n_points);
-
-        /* calculate n_iter for sigma = 1.0 */
-        n_iter = ROUND(fwhm/2.35482 * fwhm/2.35482);
-        if (n_iter == 0)
-                n_iter = 1;
-
-        /* select sigma according fwhm */
-        if (fwhm > 50.0)
-                sigma = 8.0;
-        else if (fwhm > 30.0)
-                sigma = 3.0;
-        else if (fwhm > 20.0)
-                sigma = 2.0;
-        else sigma = 1.0;
-                
-        for (j = 0; j < n_iter; j++) {
-                for (i = 0; i < polygons->n_points; i++) {
-                        heatkernel_blur_points(polygons->n_points,
-                                               polygons->points, values,
-                                               n_neighbours[i], neighbours[i],
-                                               i, sigma, NULL, &value);
-                        smooth_values[i] = value;
-                }
-                for (i = 0; i < polygons->n_points; i++)
-                        values[i] = smooth_values[i];
+                values = (double *) malloc(sizeof(double) * polygons->n_points);
+                get_smoothed_curvatures(polygons, sphere, values, fwhm,
+                                        curvtype);
         }
 
         if (sphere == NULL) {
-                /* create unit sphere with same number of triangles as skin surface */
+                /* create unit sphere w/ same # of triangles as skin surface */
                 fill_Point(centre, 0.0, 0.0, 0.0);
                 create_tetrahedral_sphere(&centre, 1.0, 1.0, 1.0,
                                   polygons->n_items, &unit_sphere);
@@ -164,14 +230,12 @@ map_smoothed_curvature_to_sphere(polygons_struct *polygons, polygons_struct *sph
         create_polygons_bintree(&unit_sphere,
                                 ROUND((double) unit_sphere.n_items *
                                       BINTREE_FACTOR));
-        
-        for (x = 0; x < size_map[0]; x++) {
-                for (y = 0; y < size_map[1]; y++) {
-                        u = ((double) x) / (double) (size_map[0] - 1);
-                        v = ((double) y) / (double) (size_map[1] - 1);
+
+        for (x = 0; x < dm[0]; x++) {
+                for (y = 0; y < dm[1]; y++) {
+                        u = ((double) x + 0.5)/(double) (dm[0]);
+                        v = ((double) y + 0.5)/(double) (dm[1]);
                         uv_to_point(u, v, &unit_point);
-                        double u2, v2;
-                        point_to_uv(&unit_point, &u2, &v2);
             
                         poly = find_closest_polygon_point(&unit_point,
                                                           &unit_sphere,
@@ -191,29 +255,26 @@ map_smoothed_curvature_to_sphere(polygons_struct *polygons, polygons_struct *sph
                                                   poly, i)];
                                 value += weights[i] * values[ind];
                         }
-                        validx = x + (size_map[0]*y);
-                        data[validx] = value;    
+                        data[x + (dm[0]*y)] = value;
                 }
         }
 
         /* scale data to uint8 range */
         mn = FLT_MAX; mx = -FLT_MAX;
-        for (i = 0; i < size_map[0]*size_map[1]; i++) {
+        for (i = 0; i < dm[0]*dm[1]; i++) {
             if (data[i] > mx) mx = data[i];
             if (data[i] < mn) mn = data[i];
         }
     
-        for (i = 0; i < size_map[0]*size_map[1]; i++) 
+        for (i = 0; i < dm[0]*dm[1]; i++) 
                 data[i] = (data[i] - mn)/(mx - mn);
         
         delete_polygons(&unit_sphere);
-        free(smooth_values);
 }
 
 void
 map_sheet2d_to_sphere(double *sheet2d, double *values,
-                      polygons_struct *polygons, int interpolate,
-                      int *size_map)
+                      polygons_struct *polygons, int interpolate, int *dm)
 {
         double               tmp_x, tmp_y;
         double               u, v;
@@ -233,32 +294,116 @@ map_sheet2d_to_sphere(double *sheet2d, double *values,
                                 ROUND((double) unit_sphere.n_items *
                                       BINTREE_FACTOR));
 
-        tmp_x = (double)size_map[0] - 1.0;
-        tmp_y = (double)size_map[1] - 1.0;
+        tmp_x = (double)dm[0];
+        tmp_y = (double)dm[1];
 
         for (i = 0; i < polygons->n_points; i++) {
                 point_to_uv(&unit_sphere.points[i], &u, &v);
 
-                x = (int) (u*tmp_x) - 1.0;
-                y = (int) (v*tmp_y) - 1.0;
-
+                x = (int) (u*tmp_x - 0.5);
+                y = (int) (v*tmp_y - 0.5);
+            
                 if (interpolate) {
-                        xp = u*tmp_x - 1.0 - x;
-                        yp = v*tmp_y - 1.0 - y;
+                        xp = u*tmp_x - 0.5 - x;
+                        yp = v*tmp_y - 0.5 - y;
                         xm = 1.0 - xp;
                         ym = 1.0 - yp;
-                        H00 = sheet2d[bound(x,  y,  size_map)];
-                        H01 = sheet2d[bound(x,  y+1,size_map)];
-                        H10 = sheet2d[bound(x+1,y,  size_map)];
-                        H11 = sheet2d[bound(x+1,y+1,size_map)];
+                        H00 = sheet2d[bound(x,  y,  dm)];
+                        H01 = sheet2d[bound(x,  y+1,dm)];
+                        H10 = sheet2d[bound(x+1,y,  dm)];
+                        H11 = sheet2d[bound(x+1,y+1,dm)];
             
                         values[i] = (ym * ( xm * H00 + xp * H10) + 
                                      yp * ( xm * H01 + xp * H11));
-                } else values[i] = sheet2d[x + y*size_map[0]];
+                } else values[i] = sheet2d[x + y*dm[0]];
                 
                 /* prevent unlikely values at the poles */
                 if (values[i] < -1e15 || values[i] > 1e15) values[i] = 0.0;
         
         }
         delete_polygons(&unit_sphere);
+}
+
+void
+map_sheet2d_to_unit_sphere(double *sheet2d, double *values,
+                           polygons_struct *sphere, int interpolate, int *dm)
+{
+        double               tmp_x, tmp_y;
+        double               u, v;
+        int                  i, x, y;
+        double               xp, yp, xm, ym;
+        double               H00, H01, H10, H11;
+        Point                unit_pt;
+
+        create_polygons_bintree(sphere, ROUND((double) sphere->n_items *
+                                      BINTREE_FACTOR));
+
+        tmp_x = (double) dm[0];
+        tmp_y = (double) dm[1];
+
+        for (i = 0; i < sphere->n_points; i++) {
+                unit_pt = sphere->points[i];
+                set_vector_length(&unit_pt, 1.0);
+                point_to_uv(&unit_pt, &u, &v);
+
+                x = (int) (u*tmp_x - 0.5);
+                y = (int) (v*tmp_y - 0.5);
+
+                if (interpolate) {
+                        xp = u*tmp_x - 0.5 - x;
+                        yp = v*tmp_y - 0.5 - y;
+                        xm = 1.0 - xp;
+                        ym = 1.0 - yp;
+                        H00 = sheet2d[bound(x,  y,  dm)];
+                        H01 = sheet2d[bound(x,  y+1,dm)];
+                        H10 = sheet2d[bound(x+1,y,  dm)];
+                        H11 = sheet2d[bound(x+1,y+1,dm)];
+            
+                        values[i] = (ym * ( xm * H00 + xp * H10) + 
+                                     yp * ( xm * H01 + xp * H11));
+                } else values[i] = sheet2d[x + y*dm[0]];
+                
+                /* prevent unlikely values at the poles */
+                if (values[i] < -1e15 || values[i] > 1e15) values[i] = 0.0;
+        
+        }
+}
+
+/* smooth the sheet such that diff between neighbours <= tol */
+void
+smooth_sheet2d(double *data, int dm[2], double tol)
+{
+        int x, y, i, done;
+        double d, x0, x1, y0, y1, *buf;
+
+        buf = (double *) malloc(sizeof(double) * dm[0] * dm[1]);
+
+        done = 0;
+        while (!done) {
+                done = 1;
+                for (x = 0; x < dm[0]; x++) {
+                        for (y = 0; y < dm[1]; y++) {
+                                i = x + dm[0]*y;
+                                buf[i] = data[i];
+
+                                x0 = data[bound(x-1,y-1,dm)];
+                                x1 = data[bound(x+1,y-1,dm)];
+                                y0 = data[bound(x-1,y+1,dm)];
+                                y1 = data[bound(x+1,y+1,dm)];
+
+                                if (fabs(x0 - data[i]) > tol ||
+                                    fabs(x1 - data[i]) > tol ||
+                                    fabs(y0 - data[i]) > tol ||
+                                    fabs(y1 - data[i]) > tol) {
+                                        buf[i] = data[i]/2.0 + (x0+x1)/8.0 +
+                                                 (y0+y1)/8.0;
+                                        done = 0;
+                                }
+                        }
+                }
+                for (x = 0; x < dm[0]*dm[1]; x++) {
+                        data[x] = buf[x];
+                }
+        }
+        free(buf);
 }
--- CAT_Map2d.h
+++ CAT_Map2d.h
@@ -17,8 +17,12 @@ typedef struct {
 	double y;
 } Vector2D;
 
-void   point_to_uv(Point *, double *, double *);
-void   uv_to_point(double, double, Point *);
-void   map_smoothed_curvature_to_sphere(polygons_struct *, polygons_struct *, double *, double *,
-                                        double, int *, int);
+void point_to_uv(Point *, double *, double *);
+void uv_to_point(double, double, Point *);
+void wrap_sheet2d(double *data, int *dm, int wrap, double *wdata);
+void unwrap_sheet2d(double *wdata, int *wdm, int wrap, double *data);
+
+void map_smoothed_curvature_to_sphere(polygons_struct *, polygons_struct *,
+                                      double *, double *, double, int *, int);
 void map_sheet2d_to_sphere(double *, double *, polygons_struct *, int, int *);
+void smooth_sheet2d(double *, int[] , double);
