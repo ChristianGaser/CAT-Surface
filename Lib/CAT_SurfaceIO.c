90c1e2f80e2053f6ff1cef64731cdfc269567cf9
Christian Gaser <christian.gaser@uni-jena.de>
2012-04-05 06:27:58 +0000

    Changed paths: !       stamp-h1 M       Progs/Makefile.am M       Lib/CAT_SurfaceIO.c M       Makefile.am
    Added fgetln function to CAT_SurfaceIO.c if glibc is defined.
    
    
    git-svn-id: svn://141.35.69.214/CAT@258 85a05d3d-4204-4b9d-9be8-300d97d0f21f

--- Lib/CAT_SurfaceIO.c
+++ Lib/CAT_SurfaceIO.c
@@ -16,6 +16,28 @@
 #define QUAD_FILE_MAGIC_NUMBER      16777215
 #define NEW_VERSION_MAGIC_NUMBER    16777215
 
+#ifdef __GLIBC__
+char *
+fgetln (stream, len)
+	FILE *stream;
+	size_t *len;
+{
+	char *line=NULL;
+	size_t nread = 0;
+
+	while (nread == 1) {
+		nread = getline (&line, len, stream);
+		if (nread == -1)
+			return NULL;
+	}
+
+	(*len)--; /* get rid of the trailing \0, fgetln
+		     does not have it */
+
+	return line;
+}
+#endif
+
 Status
 bicpl_to_facevertexdata(polygons_struct *polygons, double **faces, double **vertices)
 {
