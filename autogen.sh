6a5644ed4eec4a3927b9493328a575952da88da8
Christian Gaser <christian.gaser@uni-jena.de>
2015-09-11 19:42:58 +0000

    Changed paths: M       autogen.sh
    Added autoheader to autogen.sh
    
    
    
    git-svn-id: svn://141.35.69.214/CAT@337 85a05d3d-4204-4b9d-9be8-300d97d0f21f

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,6 @@
 
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
-libtoolize --automake --force --copy
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+autoheader
