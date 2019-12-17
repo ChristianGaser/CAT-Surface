28f0efaef57644e59d25bdb79de1216866b12a7c
Christian Gaser <christian.gaser@uni-jena.de>
2019-12-17 21:18:26 +0000

    Changed paths: A       CHANGES.txt M       INSTALL A       Include/conjugate.h M       Progs/Makefile.am M       depcomp M       mkinstalldirs
    Added: CHANGES.txt
    
    
    
    git-svn-id: svn://141.35.69.214/CAT@428 85a05d3d-4204-4b9d-9be8-300d97d0f21f

---
+++ Include/conjugate.h
@@ -0,0 +1,14 @@
+/*
+   Copyright Alan C. Evans
+   Professor of Neurology
+   McGill University
+*/
+
+#ifndef  DEF_CONJUGATE_GRADIENT_H
+#define  DEF_CONJUGATE_GRADIENT_H
+
+struct  conjugate_struct;
+
+typedef  struct  conjugate_struct  *conjugate;
+
+#endif
