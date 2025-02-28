1706844937e9c9780af8d9c3fff4e3ea4be647e6
Christian Gaser <christian.gaser@uni-jena.de>
2025-02-28 07:55:06 +0100

    Added help text to CAT_Sanlm.c

--- Progs/CAT_VolSanlm.c
+++ Progs/CAT_VolSanlm.c
@@ -25,6 +25,40 @@ ArgvInfo argTable[] = {
      {NULL, ARGV_END, NULL, NULL, NULL}
 };
 
+rivate void usage(char *executable)
+{
+    char *usage_str = "\n\
+Usage: %s [options] <input.nii> [<output_denoised.nii>]\n\
+\n\
+    This program applies a 3D Spatial-Adaptive Non-Local Means (SA-NLM) filter to\n\
+    reduce noise in medical images, particularly MRI scans. It extends the Non-Local Means\n\
+    filter by incorporating spatial adaptation to improve denoising while preserving\n\
+    structural details.\n\
+\n\
+    The filtering process includes:\n\
+\n\
+    1. **Noise Estimation:**\n\
+       - Determines noise characteristics in the image.\n\
+       - Supports both Gaussian and Rician noise models (see -rician option).\n\
+\n\
+    2. **Patch-Based Similarity Computation:**\n\
+       - Compares intensity patches across the image to find similar structures.\n\
+       - Assigns adaptive weights based on local noise variance.\n\
+\n\
+    3. **Denoising with Spatial Adaptation:**\n\
+       - Enhances edge preservation and reduces artifacts.\n\
+       - Adapts filtering strength depending on anatomical features.\n\
+\n\
+Options:\n\
+    -rician                  Use Rician noise estimation. Recommended for MRI data \n\
+                             with non-Gaussian noise distribution.\n\
+\n\
+Example:\n\
+    %s -rician input.nii output_denoised.nii\n\n";
+    
+    fprintf(stderr, usage_str, executable, executable);
+}
+
 void anlm(float* ima, int v, int f, int is_rician, const int* dims);
 
 /* Main program */
@@ -38,9 +72,9 @@ int main(int argc, char *argv[])
     nifti_image *nii_ptr;
     
     if (ParseArgv(&argc, argv, argTable, 0) ||(argc < 2)) {
-         (void) fprintf(stderr, "\nUsage: %s [options] in.nii [out.nii]\nSpatial adaptive non-local means denoising filter.\n\n", argv[0]);
-         (void) fprintf(stderr, "     %s -help\n\n", argv[0]);
-     exit(EXIT_FAILURE);
+        usage(argv[0]);
+        (void) fprintf(stderr, "     %s -help\n\n", argv[0]);
+         exit(EXIT_FAILURE);
     }
     
     infile  = argv[1];
@@ -52,8 +86,7 @@ int main(int argc, char *argv[])
         #if !defined(_WIN32) && !defined(_WIN64)
             (void) sprintf(outfile, "%s/n%s", dirname(infile), basename(infile)); 
         #else
-            fprintf(stderr,"\nUsage: %s input.nii output.nii\n\n\
-              Spatial adaptive non-local means denoising filter.\n\n", argv[0]);
+            usage(argv[0]);
             return( 1 );
         #endif
     }
