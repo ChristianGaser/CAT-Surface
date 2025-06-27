7ddf9c70c4ae2a6d179bf8a2953a4bc889b5d207
Christian Gaser <christian.gaser@uni-jena.de>
2025-06-27 15:05:23 +0200

    Added ORNLM filter

---
+++ Include/CAT_Ornlm.h
@@ -0,0 +1,17 @@
+/* Christian Gaser - christian.gaser@uni-jena.de
+ * Department of Psychiatry
+ * University of Jena
+ *
+ * Copyright Christian Gaser, University of Jena.
+ * $Id$
+ *
+ */
+
+#ifndef _CAT_ORNLM_H_
+#define _CAT_ORNLM_H_
+
+#include <math.h>
+
+void ornlm(float* ima, int v, int f, float h, const int* dims);
+
+#endif
