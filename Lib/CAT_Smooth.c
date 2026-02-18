56fb674f61ccecb4187313a35e48af610100dcd5
Christian Gaser <christian.gaser@uni-jena.de>
2026-02-18 15:58:38 +0100

    Format and style cleanup in CAT_Smooth.c
    Reformat CAT_Smooth.c for consistent coding style: normalized function declarations and parameter alignment, unified brace placement for for/if blocks, adjusted spacing around operators and commas, reflowed long lines, and minor expression tweaks (e.g. added parentheses in evaluate_heatkernel return). Also clarified early-return/free branches with explicit braces. These are layout and readability changes only; no functional logic was intentionally changed.

--- Lib/CAT_Smooth.c
+++ Lib/CAT_Smooth.c
@@ -10,7 +10,6 @@
 #include <bicpl.h>
 #include "CAT_Smooth.h"
 
-
 /**
  * \brief Extract all unique vertex neighbors for every point on a polygon mesh.
  *
@@ -22,15 +21,14 @@
  * \param n_point_neighbours_ptr (out)  int[n_points]; count of neighbors for each point
  * \param point_neighbours_ptr   (out)  int*[n_points]; neighbor vertex indices for each point
  */
-void
-get_all_polygon_point_neighbours(polygons_struct *polygons,
-                 int *n_point_neighbours_ptr[],
-                 int **point_neighbours_ptr[] )
+void get_all_polygon_point_neighbours(polygons_struct *polygons,
+                                      int *n_point_neighbours_ptr[],
+                                      int **point_neighbours_ptr[])
 {
-    int     p, poly, size, vertex;
-    int     *n_neighbours, **neighbours;
-    int     *total_neighbours, total_n_neighbours;
-    BOOLEAN   interior;
+    int p, poly, size, vertex;
+    int *n_neighbours, **neighbours;
+    int *total_neighbours, total_n_neighbours;
+    BOOLEAN interior;
 
     check_polygons_neighbours_computed(polygons);
 
@@ -40,24 +38,26 @@ get_all_polygon_point_neighbours(polygons_struct *polygons,
         n_neighbours[p] = 0;
 
     total_n_neighbours = 0;
-    for (poly = 0; poly < polygons->n_items; poly++) {
+    for (poly = 0; poly < polygons->n_items; poly++)
+    {
         size = GET_OBJECT_SIZE(*polygons, poly);
-        for (vertex = 0; vertex < size; vertex++) {
-            p = polygons->indices[
-                POINT_INDEX(polygons->end_indices,poly,vertex)];
+        for (vertex = 0; vertex < size; vertex++)
+        {
+            p = polygons->indices[POINT_INDEX(polygons->end_indices, poly, vertex)];
             if (n_neighbours[p] > 0)
                 continue;
 
             n_neighbours[p] = get_neighbours_of_point(polygons,
-                          poly, vertex, NULL, 0,
-                          &interior);
+                                                      poly, vertex, NULL, 0,
+                                                      &interior);
             total_n_neighbours += n_neighbours[p];
         }
     }
 
     ALLOC(total_neighbours, total_n_neighbours);
     total_n_neighbours = 0;
-    for (p = 0; p < polygons->n_points; p++) {
+    for (p = 0; p < polygons->n_points; p++)
+    {
         neighbours[p] = &total_neighbours[total_n_neighbours];
         total_n_neighbours += n_neighbours[p];
     }
@@ -65,22 +65,23 @@ get_all_polygon_point_neighbours(polygons_struct *polygons,
     for (p = 0; p < polygons->n_points; p++)
         n_neighbours[p] = 0;
 
-    for (poly = 0; poly < polygons->n_items; poly++) {
+    for (poly = 0; poly < polygons->n_items; poly++)
+    {
         size = GET_OBJECT_SIZE(*polygons, poly);
-        for (vertex = 0; vertex < size; vertex++) {
-            p = polygons->indices[
-                POINT_INDEX(polygons->end_indices,poly,vertex)];
+        for (vertex = 0; vertex < size; vertex++)
+        {
+            p = polygons->indices[POINT_INDEX(polygons->end_indices, poly, vertex)];
 
             if (n_neighbours[p] > 0)
                 continue;
 
             n_neighbours[p] = get_neighbours_of_point(polygons,
-                          poly, vertex, neighbours[p],
-                          total_n_neighbours,
-                          &interior);
+                                                      poly, vertex, neighbours[p],
+                                                      total_n_neighbours,
+                                                      &interior);
         }
     }
-  
+
     *n_point_neighbours_ptr = n_neighbours;
     *point_neighbours_ptr = neighbours;
 }
@@ -98,7 +99,7 @@ get_all_polygon_point_neighbours(polygons_struct *polygons,
 double
 evaluate_heatkernel(double x, double sigma)
 {
-    return(exp(-(x * x) / (2.0 * sigma * sigma)));
+    return (exp(-(x * x) / (2.0 * sigma * sigma)));
 }
 
 /**
@@ -118,61 +119,72 @@ evaluate_heatkernel(double x, double sigma)
  * \param smooth_point  (out) Point; smoothed position
  * \param value         (out) double; smoothed scalar value
  */
-void
-heatkernel_blur_points(int n_polygon_pts, Point polygon_pts[],
-             double values[], int n_neighbours, int *neighbours,
-             int ptidx, double sigma, Point *smooth_point,
-             double *value)
+void heatkernel_blur_points(int n_polygon_pts, Point polygon_pts[],
+                            double values[], int n_neighbours, int *neighbours,
+                            int ptidx, double sigma, Point *smooth_point,
+                            double *value)
 {
-    double   sum[3], weight, sum_weight, point_dist;
-    int    i, c, n_pts, neigh;
+    double sum[3], weight, sum_weight, point_dist;
+    int i, c, n_pts, neigh;
 
     if (sigma <= 0.0)
         sigma = 1e-20;
 
     sum[0] = sum[1] = sum[2] = sum_weight = 0.0;
-  
-    for (i = 0; i < n_neighbours+1; i++)
+
+    for (i = 0; i < n_neighbours + 1; i++)
         *value = 1.0;
 
-    for (i = 0; i < n_neighbours+1; i++) {
-        if (i > 0) {  /* neighbouring points */
-            neigh = neighbours[i-1];
-            if (values != NULL) {
+    for (i = 0; i < n_neighbours + 1; i++)
+    {
+        if (i > 0)
+        { /* neighbouring points */
+            neigh = neighbours[i - 1];
+            if (values != NULL)
+            {
                 point_dist = distance_between_points(
-                        &polygon_pts[ptidx],
-                        &polygon_pts[neigh]);
+                    &polygon_pts[ptidx],
+                    &polygon_pts[neigh]);
             }
-        } else {    /* center point */
+        }
+        else
+        { /* center point */
             point_dist = 0.0;
             neigh = ptidx;
         }
-        
-        if (values != NULL) {
+
+        if (values != NULL)
+        {
             /* use Gaussian kernel for values */
             weight = evaluate_heatkernel(point_dist, sigma);
-            
+
             /* only consider values that are not NaN */
-            if (!isnan(values[neigh])) {
+            if (!isnan(values[neigh]))
+            {
                 sum[0] += weight * values[neigh];
                 sum_weight += weight;
             }
-        } else {
-            /* this is rather based on empirically estimated values to be 
+        }
+        else
+        {
+            /* this is rather based on empirically estimated values to be
                compatible to older versions */
-            if (i > 0) weight = 1.0/N_DIMENSIONS;
-            else weight = 1.0;
-            
+            if (i > 0)
+                weight = 1.0 / N_DIMENSIONS;
+            else
+                weight = 1.0;
+
             for (c = 0; c < N_DIMENSIONS; c++)
                 sum[c] += weight * (double)
-                      Point_coord(polygon_pts[neigh], c);
+                                       Point_coord(polygon_pts[neigh], c);
             sum_weight += weight;
         }
     }
-    
+
     if (values != NULL)
         *value = sum[0] / sum_weight;
-    else {
+    else
+    {
         for (c = 0; c < N_DIMENSIONS; c++)
             Point_coord(*smooth_point, c) = (Point_coord_type)(sum[c] / sum_weight);
     }
@@ -190,8 +202,7 @@ heatkernel_blur_points(int n_polygon_pts, Point polygon_pts[],
  * \param values    (in/out) double[n_points]; scalar values; smoothed in-place
  * \param fwhm      (in)     Gaussian smoothing FWHM in mm (converted to sigma internally)
  */
-void
-smooth_heatkernel(polygons_struct *polygons, double *values, double fwhm)
+void smooth_heatkernel(polygons_struct *polygons, double *values, double fwhm)
 {
     double sigma, value, point_dist, sum_dist, *smooth_values;
     Point point, *smooth_pts;
@@ -199,62 +210,71 @@ smooth_heatkernel(polygons_struct *polygons, double *values, double fwhm)
     int *n_neighbours, **neighbours;
     BOOLEAN values_present;
     progress_struct progress;
-    
+
     get_all_polygon_point_neighbours(polygons, &n_neighbours, &neighbours);
-       
+
     /* estimate mean distance between the points to define optimal sigma */
     n = 0;
     sum_dist = 0.0;
-    for (i = 0; i < polygons->n_items; i++) {
-        size = GET_OBJECT_SIZE( *polygons, i );
-  
-        for (j = 0; j < size; j++) {
-            p1 = polygons->indices[POINT_INDEX(polygons->end_indices,i,j)];
-            p2 = polygons->indices[POINT_INDEX(polygons->end_indices,i,(j+1)%size)];
+    for (i = 0; i < polygons->n_items; i++)
+    {
+        size = GET_OBJECT_SIZE(*polygons, i);
+
+        for (j = 0; j < size; j++)
+        {
+            p1 = polygons->indices[POINT_INDEX(polygons->end_indices, i, j)];
+            p2 = polygons->indices[POINT_INDEX(polygons->end_indices, i, (j + 1) % size)];
 
             point_dist = distance_between_points(
-                        &polygons->points[p1],
-                        &polygons->points[p2]);
+                &polygons->points[p1],
+                &polygons->points[p2]);
             n++;
             sum_dist += point_dist;
         }
     }
 
     /* use mean distance as sigma */
-    sigma = sum_dist/(double)n;
-    
-    /* calculate n_iter with regard to fwhm   
+    sigma = sum_dist / (double)n;
+
+    /* calculate n_iter with regard to fwhm
        0.541011 equals to 3.0/(8.0*log(2.0))
        see SurfStatSmooth.m in surfstat from Keith Worsley */
-    n_iter = ceil(fwhm*fwhm*0.541011/(sigma*sigma));
-    if (n_iter < 1) n_iter = 1;
+    n_iter = ceil(fwhm * fwhm * 0.541011 / (sigma * sigma));
+    if (n_iter < 1)
+        n_iter = 1;
 
     /* recalibrate sigma to ensure integer numbers for iterations */
-    sigma = (fwhm*0.7355345)/sqrt((double)n_iter);
+    sigma = (fwhm * 0.7355345) / sqrt((double)n_iter);
 
-    initialize_progress_report(&progress, FALSE, n_iter*polygons->n_points,
-                   "Blurring");
-    if (values != NULL) 
-        smooth_values = (double *) malloc(sizeof(double) * polygons->n_points);
-    else  smooth_pts = (Point *) malloc(sizeof(Point) * polygons->n_points);
-    
-    
+    initialize_progress_report(&progress, FALSE, n_iter * polygons->n_points,
+                               "Blurring");
+    if (values != NULL)
+        smooth_values = (double *)malloc(sizeof(double) * polygons->n_points);
+    else
+        smooth_pts = (Point *)malloc(sizeof(Point) * polygons->n_points);
+
     /* diffusion smoothing using heat kernel */
-    for (j = 0; j < n_iter; j++) {
-        for (i = 0; i < polygons->n_points; i++) {
+    for (j = 0; j < n_iter; j++)
+    {
+        for (i = 0; i < polygons->n_points; i++)
+        {
             heatkernel_blur_points(polygons->n_points,
-                         polygons->points, values,
-                         n_neighbours[i], neighbours[i],
-                         i, sigma, &point, &value);
+                                   polygons->points, values,
+                                   n_neighbours[i], neighbours[i],
+                                   i, sigma, &point, &value);
 
-            if (values != NULL) {
-                if(!isnan(values[i]))
+            if (values != NULL)
+            {
+                if (!isnan(values[i]))
                     smooth_values[i] = value;
-                else  smooth_values[i] = FNAN;
-            } else    smooth_pts[i] = point;
-
+                else
+                    smooth_values[i] = FNAN;
+            }
+            else
+                smooth_pts[i] = point;
         }
-        for (i = 0; i < polygons->n_points; i++) {
+        for (i = 0; i < polygons->n_points; i++)
+        {
             if (values != NULL)
                 values[i] = smooth_values[i];
             else
@@ -264,10 +284,10 @@ smooth_heatkernel(polygons_struct *polygons, double *values, double fwhm)
 
     terminate_progress_report(&progress);
 
-    if (values != NULL) 
+    if (values != NULL)
         free(smooth_values);
-    else  free(smooth_pts);
-    
+    else
+        free(smooth_pts);
 }
 
 /**
@@ -304,7 +324,7 @@ smooth_heatkernel(polygons_struct *polygons, double *values, double fwhm)
  *
  * Implements iterative Laplacian smoothing (diffusion-like) where each vertex is
  * updated as a weighted average of itself and its neighbors. The update rule
- * is: new_vertex = (1-weight)*vertex + weight*neighbor_average. 
+ * is: new_vertex = (1-weight)*vertex + weight*neighbor_average.
  * Can optionally smooth associated scalar values using the same iterations.
  *
  * Algorithm:
@@ -319,11 +339,12 @@ smooth_heatkernel(polygons_struct *polygons, double *values, double fwhm)
  * \return              0 on success; <0 on error
  */
 int smooth_laplacian(polygons_struct *polygons,
-                         int iter,
-                         double alpha,
-                         double beta)
+                     int iter,
+                     double alpha,
+                     double beta)
 {
-    if (!polygons || polygons->n_points <= 0 || polygons->n_items <= 0) return -1;
+    if (!polygons || polygons->n_points <= 0 || polygons->n_items <= 0)
+        return -1;
 
     vec3d *V = NULL;
     vec3i *F = NULL;
@@ -336,13 +357,19 @@ int smooth_laplacian(polygons_struct *polygons,
     /* --- simplify --- */
     laplacian_smoothHC(V, F, nv, nf, alpha, beta, iter, true);
 
-    if (nv <= 0 || nf <= 0) { free(V); free(F); return -1; }
+    if (nv <= 0 || nf <= 0)
+    {
+        free(V);
+        free(F);
+        return -1;
+    }
 
     /* --- out: (V,F) -> BICPL (triangles, exclusive end_indices) --- */
     const int ok = tri_arrays_to_polygons(polygons, V, F, nv, nf);
     free(V);
     free(F);
-    if (ok != 0) return -1;
+    if (ok != 0)
+        return -1;
 
     return 0;
 }
