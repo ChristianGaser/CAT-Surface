bbbb40c6b177206b0f8f9a6d9722e5614febad85
Christian Gaser <christian.gaser@uni-jena.de>
2026-02-18 16:01:36 +0100

    Style/formatting cleanup for CAT_Kmeans.c
    Apply consistent formatting to Lib/CAT_Kmeans.c: normalize spacing around operators, align assignments, standardize brace and block placement, format loops/ifs and returns for readability. Changes are cosmetic only and intended to improve maintainability; no algorithmic or semantic behavior was modified.

--- Lib/CAT_Kmeans.c
+++ Lib/CAT_Kmeans.c
@@ -6,9 +6,9 @@
  *
  */
 
-/* This code is a substantially modified version of Tskmeans.C 
+/* This code is a substantially modified version of Tskmeans.C
  * from Jagath C. Rajapakse
- * 
+ *
  * Original author : Jagath C. Rajapakse
  *
  * See:
@@ -62,56 +62,74 @@ double EstimateKmeans(float *src, unsigned char *label, unsigned char *mask, int
     long histo[256], lut[256], cumsum[256], vol;
     double diff, dmin, dx, xnorm, sum;
 
-    vol  = dims[0]*dims[1]*dims[2];
+    vol = dims[0] * dims[1] * dims[2];
 
     /* build intensity histogram */
-    for (i = 0; i < 256; i++) histo[i] = 0;
-    for (i = 0; i < vol; i++) {
-        v = (int)ROUND(255.0*(double)src[i]/max_src);
-        if (v < 1) continue;
+    for (i = 0; i < 256; i++)
+        histo[i] = 0;
+    for (i = 0; i < vol; i++)
+    {
+        v = (int)ROUND(255.0 * (double)src[i] / max_src);
+        if (v < 1)
+            continue;
         if (mask)
             if ((thresh_mask > 0) && ((int)mask[i] < thresh_kmeans))
                 continue;
-        if (v < 0) v = 0;
-        if (v > 255) v = 255;   
-        histo[v]++;  
+        if (v < 0)
+            v = 0;
+        if (v > 255)
+            v = 255;
+        histo[v]++;
     }
 
     /* use only value in histogram where cumsum is between 1..99% */
     cumsum[0] = histo[0];
-    for (i = 1; i < 256; i++) cumsum[i] = cumsum[i-1] + histo[i];
-    for (i = 0; i < 256; i++) cumsum[i] = (long) ROUND(1000.0*(double)cumsum[i]/(double)cumsum[255]);
-    for (i = 0; i < 256; i++) if ((cumsum[i] <= 10) || (cumsum[i] >= 990)) histo[i] = 0;
+    for (i = 1; i < 256; i++)
+        cumsum[i] = cumsum[i - 1] + histo[i];
+    for (i = 0; i < 256; i++)
+        cumsum[i] = (long)ROUND(1000.0 * (double)cumsum[i] / (double)cumsum[255]);
+    for (i = 0; i < 256; i++)
+        if ((cumsum[i] <= 10) || (cumsum[i] >= 990))
+            histo[i] = 0;
 
     /* loop through */
-    diff = HUGE;    count = 0;
-    while (diff > 1.0 && count < ni) {
+    diff = HUGE;
+    count = 0;
+    while (diff > 1.0 && count < ni)
+    {
 
         /* assign class labels */
-        for (i = 0; i < 256; i++) {
+        for (i = 0; i < 256; i++)
+        {
             dmin = 256.0 * 256.0;
-            for (j = 0; j < n_classes; j++) {
-                 dx = (double) i - mean[j];
-                 dx *= dx;
-                 if (dx < dmin) {
-                     lut[i] = j;
-                     dmin = dx;
-                 }
+            for (j = 0; j < n_classes; j++)
+            {
+                dx = (double)i - mean[j];
+                dx *= dx;
+                if (dx < dmin)
+                {
+                    lut[i] = j;
+                    dmin = dx;
+                }
             }
         }
 
         /* find the new cluster centers */
         diff = 0;
-        for (i = 0; i < n_classes; i++) {
+        for (i = 0; i < n_classes; i++)
+        {
             xnorm = 0.0;
             sum = 0.0;
             for (j = 0; j < 256; j++)
-             if (lut[j] == i) {
-                 xnorm += histo[j];
-                 sum +=  j * histo[j];
-             }
-            if (xnorm > 0) sum /= xnorm;
-            else sum = 0.0;
+                if (lut[j] == i)
+                {
+                    xnorm += histo[j];
+                    sum += j * histo[j];
+                }
+            if (xnorm > 0)
+                sum /= xnorm;
+            else
+                sum = 0.0;
             dx = sum - mean[i];
             mean[i] = sum;
             dx *= dx;
@@ -121,40 +139,48 @@ double EstimateKmeans(float *src, unsigned char *label, unsigned char *mask, int
     }
 
     /* assign final labels to voxels */
-    for (i = 0; i < 256; i++) {
+    for (i = 0; i < 256; i++)
+    {
         dmin = HUGE;
         j0 = 0;
-        for (j = 0; j < n_classes; j++) {
-            if (fabs((double) i - mean[j]) < dmin) {
-                 dmin = fabs((double)i - mean[j]);
-                 j0 = j;
+        for (j = 0; j < n_classes; j++)
+        {
+            if (fabs((double)i - mean[j]) < dmin)
+            {
+                dmin = fabs((double)i - mean[j]);
+                j0 = j;
             }
         }
         lut[i] = j0;
     }
-    
+
     lut[0] = 0;
 
     /* adjust for the background label */
     diff = 0;
-    
-    for (i = 0; i < vol; i++) {
-        v = (int)ROUND(255.0*(double)src[i]/max_src);
-        if (v >= 1) {
-            if (v < 0) v = 0;
-            if (v > 255) v = 255;
-            if (label) label[i] = (unsigned char)(lut[v] + 1); 
+
+    for (i = 0; i < vol; i++)
+    {
+        v = (int)ROUND(255.0 * (double)src[i] / max_src);
+        if (v >= 1)
+        {
+            if (v < 0)
+                v = 0;
+            if (v > 255)
+                v = 255;
+            if (label)
+                label[i] = (unsigned char)(lut[v] + 1);
             diff += SQR((double)v - mean[lut[v]]);
             if (mask && label)
                 if ((thresh_mask > 0) && ((int)mask[i] < thresh_mask))
-                    label[i] = 0;   
+                    label[i] = 0;
         }
-        else
-            if (label) label[i] = 0;  
+        else if (label)
+            label[i] = 0;
     }
-    
+
     /* return square error */
-    return(diff);
+    return (diff);
 }
 
 /**
@@ -191,67 +217,81 @@ double Kmeans(float *src, unsigned char *label, unsigned char *mask, int NI, int
     double e, emin, eps, val, sum;
     double max_src = -HUGE;
     long n[n_clusters];
-    double var[MAX_NC],  mu[MAX_NC],  Mu[MAX_NC];
+    double var[MAX_NC], mu[MAX_NC], Mu[MAX_NC];
     int n_classes;
     long vol;
 
-    vol  = dims[0]*dims[1]*dims[2];
+    vol = dims[0] * dims[1] * dims[2];
 
     /* find maximum and mean inside mask */
-    for (i = 0; i < vol; i++) {
-        if (mask) {
+    for (i = 0; i < vol; i++)
+    {
+        if (mask)
+        {
             if (mask[i] > 0)
                 max_src = MAX((double)src[i], max_src);
-        } else
+        }
+        else
             max_src = MAX((double)src[i], max_src);
     }
 
     /* go through all sizes of cluster beginning with two clusters */
-    for (n_classes=2; n_classes <= n_clusters; n_classes++) {
+    for (n_classes = 2; n_classes <= n_clusters; n_classes++)
+    {
 
-        if (n_classes == 2) {
+        if (n_classes == 2)
+        {
             /* initialize for the two cluster case; */
-            n[0]=0; mean[0] = 0.0; var[0] = 0.0;
+            n[0] = 0;
+            mean[0] = 0.0;
+            var[0] = 0.0;
 
-            for (i = 0; i < vol; i++) {
-                val = 255.0*(double)src[i]/max_src;
-                if (val < 1.0/255.0) continue;
+            for (i = 0; i < vol; i++)
+            {
+                val = 255.0 * (double)src[i] / max_src;
+                if (val < 1.0 / 255.0)
+                    continue;
                 n[0]++;
                 mean[0] += val;
-                var[0]  += SQR(val);
+                var[0] += SQR(val);
             }
-            
-            Mu[0] = (n[0] != 0) ? mean[0]/n[0]: 0.0;
-            var[0] = (n[0] > 1) ? (var[0] - n[0]*Mu[0]*Mu[0])/(n[0] - 1.0) : 1.0;
-            eps = 0.5*sqrt(var[0]);
+
+            Mu[0] = (n[0] != 0) ? mean[0] / n[0] : 0.0;
+            var[0] = (n[0] > 1) ? (var[0] - n[0] * Mu[0] * Mu[0]) / (n[0] - 1.0) : 1.0;
+            eps = 0.5 * sqrt(var[0]);
         }
-        else {
+        else
+        {
             /* find the deviant (epsilon) for the node being divided */
             eps = Mu[0];
-            for (i = 0; i < n_classes-2; i++)
-                if (Mu[i+1] - Mu[i] < eps)
-                    eps = Mu[i+1] - Mu[i];
-            if (255 - Mu[n_classes-2] < eps)
-                eps = 255 - Mu[n_classes-2];
-            eps = eps*0.5;
+            for (i = 0; i < n_classes - 2; i++)
+                if (Mu[i + 1] - Mu[i] < eps)
+                    eps = Mu[i + 1] - Mu[i];
+            if (255 - Mu[n_classes - 2] < eps)
+                eps = 255 - Mu[n_classes - 2];
+            eps = eps * 0.5;
         }
-
 
         /* go through low order clustering */
         emin = HUGE;
-        for (k = 0; k < n_classes-1; k++) {
-            for (i = n_classes-1; i > k+1; i--) mean[i] = Mu[i-1];
-            mean[k+1] = Mu[k] + eps;
+        for (k = 0; k < n_classes - 1; k++)
+        {
+            for (i = n_classes - 1; i > k + 1; i--)
+                mean[i] = Mu[i - 1];
+            mean[k + 1] = Mu[k] + eps;
             mean[k] = Mu[k] - eps;
-            for (i = 1; i < k; i++) mean[i] = Mu[i];
+            for (i = 1; i < k; i++)
+                mean[i] = Mu[i];
             e = EstimateKmeans(src, label, mask, n_classes, mean, NI, dims, thresh_mask, thresh_kmeans, max_src);
-            if (e < emin) {
+            if (e < emin)
+            {
                 emin = e;
-                for (i = 0; i < n_classes; i++) 
+                for (i = 0; i < n_classes; i++)
                     mu[i] = mean[i];
             }
         }
-        for (i = 0; i < n_classes; i++) Mu[i] = mu[i];       
+        for (i = 0; i < n_classes; i++)
+            Mu[i] = mu[i];
     }
 
     e = EstimateKmeans(src, label, mask, n_clusters, mu, NI, dims, thresh_mask, thresh_kmeans, max_src);
@@ -260,20 +300,8 @@ double Kmeans(float *src, unsigned char *label, unsigned char *mask, int NI, int
     for (i = 0; i < vol; i++)
         max_src = MAX((double)src[i], max_src);
 
-    for (i = 0; i < n_classes; i++) mean[i] = max_src*mu[i]/255.0;       
-    
-    return(max_src);        
+    for (i = 0; i < n_classes; i++)
+        mean[i] = max_src * mu[i] / 255.0;
+
+    return (max_src);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
